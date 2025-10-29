6dad828b80be70dbd9196b123c64f2acfae2f779
lgztx <z2337878191@outlook.com>
2025-10-30 01:49:12 +0800

    Add ListViewExtensions

--- CommunityToolkit.WinUI/Extensions/ListViewBase/ListViewExtensions.cpp
+++ CommunityToolkit.WinUI/Extensions/ListViewBase/ListViewExtensions.cpp
@@ -3,36 +3,462 @@
 #if __has_include("ListViewExtensions.g.cpp")
 #include "ListViewExtensions.g.cpp"
 #endif
+#include "../common.h"
 
 using namespace winrt::Microsoft::UI::Xaml::Data;
 
 namespace winrt::CommunityToolkit::WinUI::implementation
 {
-    void ListViewExtensions::DeselectAll(ListViewBase const& list)
-    {
-        switch (list.SelectionMode())
-        {
-        case ListViewSelectionMode::Single:
-            list.SelectedItem(nullptr);
-            break;
-        case ListViewSelectionMode::Multiple:
-        case ListViewSelectionMode::Extended:
-            list.DeselectRange(ItemIndexRange(0, list.Items().Size()));
-            break;
-        }
-    }
+	void ListViewExtensions::OnAlternateColorPropertyChanged(DependencyObject const& sender, [[maybe_unused]] DependencyPropertyChangedEventArgs const& args)
+	{
+		if (auto listViewBase = sender.try_as<ListViewBase>())
+		{
+			auto items = listViewBase.Items().try_as<IObservableVector<IInspectable>>();
+			if (const auto& iter = _itemsForList.find(items); iter != _itemsForList.end())
+			{
+				_itemsForList.erase(iter);
+			}
 
-    void ListViewExtensions::SelectAllSafe(ListViewBase const& list)
-    {
-        switch (list.SelectionMode())
-        {
-        case ListViewSelectionMode::Single:
-            list.SelectedItem(list.Items().Size() > 0 ? list.Items().GetAt(0) : nullptr);
-            break;
-        case ListViewSelectionMode::Multiple:
-        case ListViewSelectionMode::Extended:
-            list.SelectRange(ItemIndexRange(0, list.Items().Size()));
-            break;
-        }
-    }
+			auto ctx = std::make_unique<EventContext>();
+			ctx->_listViewBase = listViewBase;
+
+			if (AlternateColorProperty != nullptr)
+			{
+				ctx->_colorContentChangingRevoker = listViewBase.ContainerContentChanging(winrt::auto_revoke, { &ListViewExtensions::ColorContainerContentChanging });
+				ctx->_itemsVectorChangedRevoker = listViewBase.Items().VectorChanged(winrt::auto_revoke, &ListViewExtensions::ColorItemsVectorChanged);
+				ctx->_unloadedRevoker = listViewBase.Unloaded(winrt::auto_revoke, &ListViewExtensions::OnListViewBaseUnloaded);
+			}
+
+			_itemsForList.try_emplace(items, std::move(ctx));
+		}
+	}
+
+	void ListViewExtensions::ColorContainerContentChanging(ListViewBase const& sender, ContainerContentChangingEventArgs const& args)
+	{
+		auto itemContainer = args.ItemContainer().try_as<Control>();
+		SetItemContainerBackground(sender, itemContainer, args.ItemIndex());
+	}
+
+	void ListViewExtensions::OnAlternateItemTemplatePropertyChanged(DependencyObject const& sender, [[maybe_unused]] DependencyPropertyChangedEventArgs const& args)
+	{
+		if (auto listViewBase = sender.try_as<ListViewBase>())
+		{
+			for (const auto& [_, ctx] : _itemsForList)
+			{
+				if (ctx->_listViewBase == listViewBase)
+				{
+					ctx->_itemTemplateContentChangingRevoker.revoke();
+					ctx->_unloadedRevoker.revoke();
+
+					if (AlternateItemTemplateProperty != nullptr)
+					{
+						ctx->_itemTemplateContentChangingRevoker = listViewBase.ContainerContentChanging(winrt::auto_revoke, &ListViewExtensions::ItemTemplateContainerContentChanging);
+						ctx->_unloadedRevoker = listViewBase.Unloaded(winrt::auto_revoke, &ListViewExtensions::OnListViewBaseUnloaded);
+					}
+
+					break;
+				}
+			}
+		}
+	}
+
+	void ListViewExtensions::ItemTemplateContainerContentChanging(ListViewBase const& sender, ContainerContentChangingEventArgs const& args)
+	{
+		if (args.ItemIndex() % 2 == 0)
+		{
+			args.ItemContainer().ContentTemplate(GetAlternateItemTemplate(sender));
+		}
+		else
+		{
+			args.ItemContainer().ContentTemplate(sender.ItemTemplate());
+		}
+	}
+
+	void ListViewExtensions::OnItemContainerStretchDirectionPropertyChanged(DependencyObject const& sender, [[maybe_unused]] DependencyPropertyChangedEventArgs const& args)
+	{
+		if (auto listViewBase = sender.try_as<ListViewBase>())
+		{
+			for (const auto& [_, ctx] : _itemsForList) {
+				if (ctx->_listViewBase == listViewBase)
+				{
+					ctx->_stretchDirectionContentChangingRevoker.revoke();
+					ctx->_unloadedRevoker.revoke();
+
+					if (ItemContainerStretchDirectionProperty != nullptr)
+					{
+						ctx->_stretchDirectionContentChangingRevoker = listViewBase.ContainerContentChanging(winrt::auto_revoke, &ListViewExtensions::ItemContainerStretchDirectionChanging);
+						ctx->_unloadedRevoker = listViewBase.Unloaded(winrt::auto_revoke, &ListViewExtensions::OnListViewBaseUnloaded);
+					}
+
+					break;
+				}
+			}
+		}
+	}
+
+	void ListViewExtensions::ItemContainerStretchDirectionChanging(ListViewBase const& sender, ContainerContentChangingEventArgs const& args)
+	{
+		auto stretchDirection = GetItemContainerStretchDirection(sender);
+
+		if (stretchDirection == ItemContainerStretchDirection::Vertical || stretchDirection == ItemContainerStretchDirection::Both)
+		{
+			args.ItemContainer().VerticalContentAlignment(VerticalAlignment::Stretch);
+		}
+
+		if (stretchDirection == ItemContainerStretchDirection::Horizontal || stretchDirection == ItemContainerStretchDirection::Both)
+		{
+			args.ItemContainer().HorizontalContentAlignment(HorizontalAlignment::Stretch);
+		}
+	}
+
+	void ListViewExtensions::OnListViewBaseUnloaded(IInspectable const& sender, [[maybe_unused]] RoutedEventArgs const& e)
+	{
+		if (auto listViewBase = sender.try_as<ListViewBase>())
+		{
+			_itemsForList.erase(listViewBase.Items());
+		}
+	}
+
+	void ListViewExtensions::ColorItemsVectorChanged(IObservableVector<IInspectable> const& sender, IVectorChangedEventArgs const& args)
+	{
+		// If the index is at the end we can ignore
+		if (args.Index() == (sender.Size() - 1))
+		{
+			return;
+		}
+
+		// Only need to handle Inserted and Removed because we'll handle everything else in the
+		// ColorContainerContentChanging method
+		if ((args.CollectionChange() == CollectionChange::ItemInserted) || (args.CollectionChange() == CollectionChange::ItemRemoved))
+		{
+			auto iter = _itemsForList.find(sender);
+			if (iter == _itemsForList.end())
+			{
+				return;
+			}
+
+			if (const auto& listViewBase = iter->second->_listViewBase)
+			{
+				uint32_t index = args.Index();
+				for (uint32_t i = index; i < sender.Size(); i++)
+				{
+					auto itemContainer = listViewBase.ContainerFromIndex(i).try_as<Control>();
+					if (itemContainer != nullptr)
+					{
+						SetItemContainerBackground(listViewBase, itemContainer, i);
+					}
+				}
+			}
+		}
+	}
+
+	void ListViewExtensions::SetItemContainerBackground(ListViewBase const& sender, Control const& itemContainer, int itemIndex)
+	{
+		if (itemIndex % 2 == 0)
+		{
+			itemContainer.Background(GetAlternateColor(sender));
+			auto rootBorder = DependencyObjectEx::FindDescendant<Border>(itemContainer);
+			if (rootBorder != nullptr)
+			{
+				rootBorder.Background(GetAlternateColor(sender));
+			}
+		}
+		else
+		{
+			itemContainer.Background(nullptr);
+			auto rootBorder = DependencyObjectEx::FindDescendant<Border>(itemContainer);
+			if (rootBorder != nullptr)
+			{
+				rootBorder.Background(nullptr);
+			}
+		}
+	}
+
+	void ListViewExtensions::DeselectAll(ListViewBase const& list)
+	{
+		switch (list.SelectionMode())
+		{
+		case ListViewSelectionMode::Single:
+			list.SelectedItem(nullptr);
+			break;
+		case ListViewSelectionMode::Multiple:
+		case ListViewSelectionMode::Extended:
+			list.DeselectRange(ItemIndexRange(0, list.Items().Size()));
+			break;
+		}
+	}
+
+	void ListViewExtensions::SelectAllSafe(ListViewBase const& list)
+	{
+		switch (list.SelectionMode())
+		{
+		case ListViewSelectionMode::Single:
+			list.SelectedItem(list.Items().Size() > 0 ? list.Items().GetAt(0) : nullptr);
+			break;
+		case ListViewSelectionMode::Multiple:
+		case ListViewSelectionMode::Extended:
+			list.SelectRange(ItemIndexRange(0, list.Items().Size()));
+			break;
+		}
+	}
+
+	void ListViewExtensions::OnCommandPropertyChanged(DependencyObject const& sender, DependencyPropertyChangedEventArgs const& args)
+	{
+		auto listViewBase = sender.try_as<ListViewBase>();
+
+		if (listViewBase == nullptr)
+		{
+			return;
+		}
+
+		if (auto iter = _commandTokens.find(listViewBase); iter != _commandTokens.end())
+		{
+			_commandTokens.erase(iter);
+		}
+
+		if (auto oldCommand = args.OldValue().try_as<ICommand>())
+		{
+
+		}
+
+		if (auto newCommand = args.NewValue().try_as<ICommand>())
+		{
+			auto revoker = std::make_unique<ListViewBase::ItemClick_revoker>();
+			*revoker = listViewBase.ItemClick(winrt::auto_revoke, &ListViewExtensions::OnListViewBaseItemClick);
+			
+			_commandTokens.emplace(listViewBase, std::move(revoker));
+		}
+	}
+
+	void ListViewExtensions::OnListViewBaseItemClick(IInspectable const& sender, ItemClickEventArgs const& e)
+	{
+		if (auto listViewBase = sender.try_as<ListViewBase>())
+		{
+			auto command = GetCommand(listViewBase);
+			if (listViewBase == nullptr || command == nullptr)
+			{
+				return;
+			}
+
+			if (command.CanExecute(e.ClickedItem()))
+			{
+				command.Execute(e.ClickedItem());
+			}
+		}
+	}
+
+	IAsyncAction ListViewExtensions::SmoothScrollIntoViewWithIndexAsync(ListViewBase const& listViewBase, int index, ScrollItemPlacement itemPlacement, bool disableAnimation, bool scrollIfVisible, int additionalHorizontalOffset, int additionalVerticalOffset)
+	{
+		auto items = listViewBase.Items();
+		if (index > (static_cast<int>(items.Size()) - 1))
+		{
+			index = items.Size() - 1;
+		}
+
+		if (index < -static_cast<int>(items.Size()))
+		{
+			index = -static_cast<int>(items.Size());
+		}
+
+		index = (index < 0) ? (index + items.Size()) : index;
+
+		bool isVirtualizing = false;
+		double previousXOffset = false, previousYOffset = false;
+
+		auto scrollViewer = DependencyObjectEx::FindDescendant<ScrollViewer>(listViewBase);
+		auto selectorItem = listViewBase.ContainerFromIndex(index).try_as<SelectorItem>();
+
+		if (scrollViewer == nullptr)
+		{
+			co_return;
+		}
+
+		// If selectorItem is null then the panel is virtualized.
+		// So in order to get the container of the item we need to scroll to that item first and then use ContainerFromIndex
+		if (selectorItem == nullptr)
+		{
+			isVirtualizing = true;
+
+			previousXOffset = scrollViewer.HorizontalOffset();
+			previousYOffset = scrollViewer.VerticalOffset();
+
+			auto tcs = std::make_shared<winrt::handle>(CreateEventW(nullptr, true, false, nullptr));
+			ScrollViewer::ViewChanged_revoker viewChangedRevoker;
+			auto ViewChanged = [tcs](IInspectable const&, ScrollViewerViewChangedEventArgs const&)
+				{
+					SetEvent(tcs->get());
+				};
+
+			try
+			{
+				viewChangedRevoker = scrollViewer.ViewChanged(winrt::auto_revoke, ViewChanged);
+				listViewBase.ScrollIntoView(items.GetAt(index), ScrollIntoViewAlignment::Leading);
+				co_await winrt::resume_on_signal(tcs->get());
+			}
+			catch (...)
+			{
+
+			}
+
+			selectorItem = listViewBase.ContainerFromIndex(index).try_as<SelectorItem>();
+		}
+
+		auto transform = selectorItem.TransformToVisual(scrollViewer.Content().try_as<UIElement>());
+		auto position = transform.TransformPoint(Point(0, 0));
+
+		// Scrolling back to previous position
+		if (isVirtualizing)
+		{
+			co_await ChangeViewAsync(scrollViewer, previousXOffset, previousYOffset, std::nullopt, true);
+		}
+
+		auto listViewBaseWidth = listViewBase.ActualWidth();
+		auto selectorItemWidth = selectorItem.ActualWidth();
+		auto listViewBaseHeight = listViewBase.ActualHeight();
+		auto selectorItemHeight = selectorItem.ActualHeight();
+
+		previousXOffset = scrollViewer.HorizontalOffset();
+		previousYOffset = scrollViewer.VerticalOffset();
+
+		auto minXPosition = position.X - listViewBaseWidth + selectorItemWidth;
+		auto minYPosition = position.Y - listViewBaseHeight + selectorItemHeight;
+
+		auto maxXPosition = position.X;
+		auto maxYPosition = position.Y;
+
+		double finalXPosition, finalYPosition;
+
+		// If the Item is in view and scrollIfVisible is false then we don't need to scroll
+		if (!scrollIfVisible && (previousXOffset <= maxXPosition && previousXOffset >= minXPosition) && (previousYOffset <= maxYPosition && previousYOffset >= minYPosition))
+		{
+			finalXPosition = previousXOffset;
+			finalYPosition = previousYOffset;
+		}
+		else
+		{
+			switch (itemPlacement)
+			{
+			case ScrollItemPlacement::Default:
+				if (previousXOffset <= maxXPosition && previousXOffset >= minXPosition)
+				{
+					finalXPosition = previousXOffset + additionalHorizontalOffset;
+				}
+				else if (std::abs(previousXOffset - minXPosition) < std::abs(previousXOffset - maxXPosition))
+				{
+					finalXPosition = minXPosition + additionalHorizontalOffset;
+				}
+				else
+				{
+					finalXPosition = maxXPosition + additionalHorizontalOffset;
+				}
+
+				if (previousYOffset <= maxYPosition && previousYOffset >= minYPosition)
+				{
+					finalYPosition = previousYOffset + additionalVerticalOffset;
+				}
+				else if (std::abs(previousYOffset - minYPosition) < std::abs(previousYOffset - maxYPosition))
+				{
+					finalYPosition = minYPosition + additionalVerticalOffset;
+				}
+				else
+				{
+					finalYPosition = maxYPosition + additionalVerticalOffset;
+				}
+
+				break;
+
+			case ScrollItemPlacement::Left:
+				finalXPosition = maxXPosition + additionalHorizontalOffset;
+				finalYPosition = previousYOffset + additionalVerticalOffset;
+				break;
+
+			case ScrollItemPlacement::Top:
+				finalXPosition = previousXOffset + additionalHorizontalOffset;
+				finalYPosition = maxYPosition + additionalVerticalOffset;
+				break;
+
+			case ScrollItemPlacement::Center:
+			{
+				auto centreX = (listViewBaseWidth - selectorItemWidth) / 2.0;
+				auto centreY = (listViewBaseHeight - selectorItemHeight) / 2.0;
+				finalXPosition = maxXPosition - centreX + additionalHorizontalOffset;
+				finalYPosition = maxYPosition - centreY + additionalVerticalOffset;
+				break;
+			}
+			case ScrollItemPlacement::Right:
+				finalXPosition = minXPosition + additionalHorizontalOffset;
+				finalYPosition = previousYOffset + additionalVerticalOffset;
+				break;
+
+			case ScrollItemPlacement::Bottom:
+				finalXPosition = previousXOffset + additionalHorizontalOffset;
+				finalYPosition = minYPosition + additionalVerticalOffset;
+				break;
+
+			default:
+				finalXPosition = previousXOffset + additionalHorizontalOffset;
+				finalYPosition = previousYOffset + additionalVerticalOffset;
+				break;
+			}
+		}
+
+		co_await ChangeViewAsync(scrollViewer, finalXPosition, finalYPosition, std::nullopt, disableAnimation);
+	}
+
+	IAsyncAction ListViewExtensions::SmoothScrollIntoViewWithItemAsync(ListViewBase const& listViewBase, IInspectable const& item, ScrollItemPlacement itemPlacement, bool disableAnimation, bool scrollIfVisible, int additionalHorizontalOffset, int additionalVerticalOffset)
+	{
+		uint32_t index;
+		if (listViewBase.Items().IndexOf(item, index))
+		{
+			co_await SmoothScrollIntoViewWithIndexAsync(listViewBase, index, itemPlacement, disableAnimation, scrollIfVisible, additionalHorizontalOffset, additionalVerticalOffset);
+		}
+	}
+
+	IAsyncAction ListViewExtensions::ChangeViewAsync(ScrollViewer const& scrollViewer, std::optional<double> horizontalOffset, std::optional<double> verticalOffset, std::optional<float> zoomFactor, bool disableAnimation)
+	{
+		if (horizontalOffset > scrollViewer.ScrollableWidth())
+		{
+			horizontalOffset = scrollViewer.ScrollableWidth();
+		}
+		else if (horizontalOffset < 0)
+		{
+			horizontalOffset = 0;
+		}
+
+		if (verticalOffset > scrollViewer.ScrollableHeight())
+		{
+			verticalOffset = scrollViewer.ScrollableHeight();
+		}
+		else if (verticalOffset < 0)
+		{
+			verticalOffset = 0;
+		}
+
+		// MUST check this and return immediately, otherwise this async task will never complete because ViewChanged event won't get triggered
+		if (horizontalOffset == scrollViewer.HorizontalOffset() && verticalOffset == scrollViewer.VerticalOffset())
+		{
+			co_return;
+		}
+
+		auto tcs = std::make_shared<winrt::handle>(CreateEventW(nullptr, true, false, nullptr));
+		ScrollViewer::ViewChanged_revoker viewChangedRevoker;
+		auto ViewChanged = [tcs](IInspectable const&, ScrollViewerViewChangedEventArgs const& e)
+			{
+				if (!e.IsIntermediate())
+				{
+					SetEvent(tcs->get());
+				}
+			};
+
+		try
+		{
+			viewChangedRevoker = scrollViewer.ViewChanged(winrt::auto_revoke, ViewChanged);
+			scrollViewer.ChangeView(horizontalOffset, verticalOffset, zoomFactor, disableAnimation);
+			co_await winrt::resume_on_signal(tcs->get());
+		}
+		catch (...)
+		{
+
+		}
+	}
 }
--- CommunityToolkit.WinUI/Extensions/ListViewBase/ListViewExtensions.h
+++ CommunityToolkit.WinUI/Extensions/ListViewBase/ListViewExtensions.h
@@ -4,17 +4,152 @@
 
 namespace winrt::CommunityToolkit::WinUI::implementation
 {
-    struct ListViewExtensions : ListViewExtensionsT<ListViewExtensions>
-    {
-        static void DeselectAll(ListViewBase const& list);
+	using namespace winrt::Windows::Foundation::Collections;
+	using namespace winrt::Microsoft::UI::Xaml::Media;
 
-        static void SelectAllSafe(ListViewBase const& list);
-    };
+	struct ListViewExtensions : ListViewExtensionsT<ListViewExtensions>
+	{
+		static void OnItemContainerStretchDirectionPropertyChanged(
+			Microsoft::UI::Xaml::DependencyObject const& d,
+			Microsoft::UI::Xaml::DependencyPropertyChangedEventArgs const& e);
+
+		static inline const wil::single_threaded_property<winrt::Microsoft::UI::Xaml::DependencyProperty> ItemContainerStretchDirectionProperty =
+			winrt::Microsoft::UI::Xaml::DependencyProperty::RegisterAttached(
+				L"ItemContainerStretchDirection",
+				winrt::xaml_typename<ItemContainerStretchDirection>(),
+				winrt::xaml_typename<class_type>(),
+				winrt::Microsoft::UI::Xaml::PropertyMetadata{ winrt::box_value(false), &ListViewExtensions::OnItemContainerStretchDirectionPropertyChanged });
+
+		static ItemContainerStretchDirection GetItemContainerStretchDirection(
+			Microsoft::UI::Xaml::Controls::ListViewBase const& obj)
+		{
+			return obj.GetValue(ItemContainerStretchDirectionProperty()).as<ItemContainerStretchDirection>();
+		}
+
+		static void SetItemContainerStretchDirection(
+			Microsoft::UI::Xaml::Controls::ListViewBase const& obj,
+			ItemContainerStretchDirection const& value)
+		{
+			obj.SetValue(ItemContainerStretchDirectionProperty(), winrt::box_value(value));
+		}
+
+		static void DeselectAll(ListViewBase const& list);
+
+		static void SelectAllSafe(ListViewBase const& list);
+
+#pragma region AlternateRows
+	private:
+
+		struct EventContext
+		{
+			EventContext() = default;
+			ListViewBase _listViewBase{ nullptr };
+			ListViewBase::ContainerContentChanging_revoker _colorContentChangingRevoker;
+			ListViewBase::ContainerContentChanging_revoker _itemTemplateContentChangingRevoker;
+			ListViewBase::ContainerContentChanging_revoker _stretchDirectionContentChangingRevoker;
+			IObservableVector<IInspectable>::VectorChanged_revoker _itemsVectorChangedRevoker;
+			FrameworkElement::Unloaded_revoker _unloadedRevoker;
+		};
+
+		static inline std::unordered_map<IObservableVector<IInspectable>, std::unique_ptr<EventContext>> _itemsForList;
+
+	public:
+		static void OnAlternateColorPropertyChanged(DependencyObject const& sender, DependencyPropertyChangedEventArgs const& args);
+
+		static void OnAlternateItemTemplatePropertyChanged(DependencyObject const& sender, DependencyPropertyChangedEventArgs const& args);
+
+		static inline const wil::single_threaded_property<winrt::Microsoft::UI::Xaml::DependencyProperty> AlternateColorProperty
+			= DependencyProperty::RegisterAttached(
+				L"AlternateColor",
+				winrt::xaml_typename<Brush>(),
+				winrt::xaml_typename<class_type>(),
+				PropertyMetadata(nullptr, &ListViewExtensions::OnAlternateColorPropertyChanged));
+
+		static inline const wil::single_threaded_property<winrt::Microsoft::UI::Xaml::DependencyProperty> AlternateItemTemplateProperty
+			= DependencyProperty::RegisterAttached(
+				L"AlternateItemTemplate",
+				winrt::xaml_typename<DataTemplate>(),
+				winrt::xaml_typename<class_type>(),
+				PropertyMetadata(nullptr, &ListViewExtensions::OnAlternateItemTemplatePropertyChanged));
+
+		static Brush GetAlternateColor(ListViewBase const& obj)
+		{
+			return obj.GetValue(AlternateColorProperty).try_as<Brush>();
+		}
+
+		static void SetAlternateColor(ListViewBase const& obj, Brush const& value)
+		{
+			obj.SetValue(AlternateColorProperty, value);
+		}
+
+		static DataTemplate GetAlternateItemTemplate(ListViewBase const& obj)
+		{
+			return obj.GetValue(AlternateItemTemplateProperty).try_as<DataTemplate>();
+		}
+
+		static void SetAlternateItemTemplate(ListViewBase const& obj, DataTemplate const& value)
+		{
+			obj.SetValue(AlternateItemTemplateProperty, value);
+		}
+
+	private:
+		static void ColorContainerContentChanging(ListViewBase const& sender, ContainerContentChangingEventArgs const& args);
+
+		static void ItemTemplateContainerContentChanging(ListViewBase const& sender, ContainerContentChangingEventArgs const& args);
+
+		static void ItemContainerStretchDirectionChanging(ListViewBase const& sender, ContainerContentChangingEventArgs const& args);
+
+		static void OnListViewBaseUnloaded(IInspectable const& sender, RoutedEventArgs const& e);
+
+		static void ColorItemsVectorChanged(IObservableVector<IInspectable> const& sender, IVectorChangedEventArgs const& args);
+
+		static void SetItemContainerBackground(ListViewBase const& sender, Control const& itemContainer, int itemIndex);
+#pragma endregion
+
+
+#pragma region Command
+	public:
+		static void OnCommandPropertyChanged(DependencyObject const& sender, DependencyPropertyChangedEventArgs const& args);
+
+		static inline const wil::single_threaded_property<winrt::Microsoft::UI::Xaml::DependencyProperty> CommandProperty
+			= DependencyProperty::RegisterAttached(
+				L"Command",
+				winrt::xaml_typename<ICommand>(),
+				winrt::xaml_typename<class_type>(),
+				PropertyMetadata(nullptr, &ListViewExtensions::OnCommandPropertyChanged));
+
+		static ICommand GetCommand(ListViewBase obj)
+		{
+			return obj.GetValue(CommandProperty).try_as<ICommand>();
+		}
+
+		static void SetCommand(ListViewBase const& obj, ICommand const& value)
+		{
+			obj.SetValue(CommandProperty, value);
+		}
+
+	private:
+		static inline std::unordered_map<ListViewBase, std::unique_ptr<ListViewBase::ItemClick_revoker>> _commandTokens;
+
+		static void OnListViewBaseItemClick(IInspectable const& sender, ItemClickEventArgs const& e);
+#pragma endregion
+
+#pragma region SmoothScrollIntoView
+	public:
+		static IAsyncAction SmoothScrollIntoViewWithIndexAsync(ListViewBase const& listViewBase, int index, ScrollItemPlacement itemPlacement = ScrollItemPlacement::Default, bool disableAnimation = false, bool scrollIfVisible = true, int additionalHorizontalOffset = 0, int additionalVerticalOffset = 0);
+
+
+		static IAsyncAction SmoothScrollIntoViewWithItemAsync(ListViewBase const& listViewBase, IInspectable const& item, ScrollItemPlacement itemPlacement = ScrollItemPlacement::Default, bool disableAnimation = false, bool scrollIfVisible = true, int additionalHorizontalOffset = 0, int additionalVerticalOffset = 0);
+
+	private:
+		static IAsyncAction ChangeViewAsync(ScrollViewer const& scrollViewer, std::optional<double> horizontalOffset, std::optional<double> verticalOffset, std::optional<float> zoomFactor, bool disableAnimation);
+#pragma endregion
+	};
 }
 
 namespace winrt::CommunityToolkit::WinUI::factory_implementation
 {
-    struct ListViewExtensions : ListViewExtensionsT<ListViewExtensions, implementation::ListViewExtensions>
-    {
-    };
+	struct ListViewExtensions : ListViewExtensionsT<ListViewExtensions, implementation::ListViewExtensions>
+	{
+	};
 }
