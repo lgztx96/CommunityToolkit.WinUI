82a4a120556043943e70fe06e49dc17a5f9056ca
lgztx <z2337878191@outlook.com>
2026-04-21 21:36:19 +0800

    Add Command and CommandParameter support for Hyperlink via HyperlinkExtensions

---
+++ XamlToolkit.WinUI/Extensions/Text/HyperlinkExtensions.cpp
@@ -0,0 +1,84 @@
+﻿#include "pch.h"
+#include "HyperlinkExtensions.h"
+#if __has_include("HyperlinkExtensions.g.cpp")
+#include "HyperlinkExtensions.g.cpp"
+#endif
+
+namespace winrt::XamlToolkit::WinUI::implementation
+{
+    const wil::single_threaded_property<winrt::DependencyProperty> HyperlinkExtensions::CommandProperty =
+        DependencyProperty::RegisterAttached(
+            L"Command",
+            winrt::xaml_typename<winrt::ICommand>(),
+            winrt::xaml_typename<winrt::XamlToolkit::WinUI::HyperlinkExtensions>(),
+            winrt::PropertyMetadata(nullptr, OnCommandPropertyChanged));
+
+    const wil::single_threaded_property<winrt::DependencyProperty> HyperlinkExtensions::CommandParameterProperty =
+        DependencyProperty::RegisterAttached(
+            L"CommandParameter",
+            winrt::xaml_typename<winrt::IInspectable>(),
+            winrt::xaml_typename<winrt::XamlToolkit::WinUI::HyperlinkExtensions>(),
+            winrt::PropertyMetadata(nullptr));
+
+    const wil::single_threaded_property<winrt::DependencyProperty> HyperlinkExtensions::ClickEventTokenProperty =
+        DependencyProperty::RegisterAttached(
+            L"ClickToken",
+            winrt::xaml_typename<winrt::IInspectable>(),
+            winrt::xaml_typename<winrt::XamlToolkit::WinUI::HyperlinkExtensions>(),
+            winrt::PropertyMetadata(nullptr));
+
+    winrt::ICommand HyperlinkExtensions::GetCommand(winrt::Hyperlink const& obj)
+    {
+        return obj.GetValue(CommandProperty()).try_as<winrt::ICommand>();
+    }
+
+    void HyperlinkExtensions::SetCommand(winrt::Hyperlink const& obj, winrt::ICommand const& value)
+    {
+        obj.SetValue(CommandProperty(), value);
+    }
+
+    winrt::IInspectable HyperlinkExtensions::GetCommandParameter(winrt::Hyperlink const& obj) 
+    { 
+        return obj.GetValue(CommandParameterProperty());
+    }
+
+    void HyperlinkExtensions::SetCommandParameter(winrt::Hyperlink const& obj, winrt::IInspectable const& value) 
+    {
+        obj.SetValue(CommandParameterProperty(), value);
+    }
+
+    void HyperlinkExtensions::OnCommandPropertyChanged(winrt::DependencyObject const& sender, winrt::DependencyPropertyChangedEventArgs const& args)
+    {
+        if (const auto hyperlink = sender.try_as<winrt::Hyperlink>())
+        {
+            if (const auto value = hyperlink.GetValue(ClickEventTokenProperty()))
+            {
+                if (const auto token = value.try_as<winrt::event_token>())
+                {
+                    hyperlink.Click(*token);
+                }
+            }
+
+            if (args.NewValue().try_as<winrt::ICommand>())
+            {
+                auto token = hyperlink.Click(&HyperlinkExtensions::OnHyperlinkClicked);
+                hyperlink.SetValue(ClickEventTokenProperty(), winrt::box_value(token));
+            }
+            else
+            {
+                hyperlink.ClearValue(ClickEventTokenProperty());
+            }
+        }
+    }
+
+    void HyperlinkExtensions::OnHyperlinkClicked(winrt::Hyperlink const& sender, [[maybe_unused]] winrt::HyperlinkClickEventArgs const& args)
+    {
+        auto command = GetCommand(sender);
+        auto parameter = GetCommandParameter(sender);
+
+        if (command && command.CanExecute(parameter))
+        {
+            command.Execute(parameter);
+        }
+    }
+}
---
+++ XamlToolkit.WinUI/Extensions/Text/HyperlinkExtensions.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+
+#include "HyperlinkExtensions.g.h"
+#include <wil/wistd_type_traits.h>
+#include <wil/cppwinrt_authoring.h>
+
+namespace winrt
+{
+    using namespace Microsoft::UI::Xaml;
+    using namespace Microsoft::UI::Xaml::Input;
+    using namespace Microsoft::UI::Xaml::Documents;
+    using namespace Windows::Foundation;
+}
+
+namespace winrt::XamlToolkit::WinUI::implementation
+{
+    struct HyperlinkExtensions
+    {
+        /// <summary>
+        /// Attached <see cref="DependencyProperty"/> for binding an <see cref="ICommand"/> instance to a <see cref="Hyperlink"/>
+        /// </summary>
+        static const wil::single_threaded_property<winrt::DependencyProperty> CommandProperty;
+
+        /// <summary>
+        /// Attached <see cref="DependencyProperty"/> for binding a command parameter to a <see cref="Hyperlink"/>
+        /// </summary>
+        static const wil::single_threaded_property<winrt::DependencyProperty> CommandParameterProperty;
+
+        /// <summary>
+        /// Gets the <see cref="ICommand"/> instance associated with the specified <see cref="Hyperlink"/>
+        /// </summary>
+        /// <param name="obj">The <see cref="Hyperlink"/> from which to get the associated <see cref="ICommand"/> instance</param>
+        /// <returns>The <see cref="ICommand"/> instance associated with the <see cref="Hyperlink"/> or null</returns>
+        static winrt::ICommand GetCommand(winrt::Hyperlink const& obj);
+
+        /// <summary>
+        /// Sets the <see cref="ICommand"/> instance associated with the specified <see cref="Hyperlink"/>
+        /// </summary>
+        /// <param name="obj">The <see cref="Hyperlink"/> to associated the <see cref="ICommand"/> instance to</param>
+        /// <param name="value">The <see cref="ICommand"/> instance to bind to the <see cref="Hyperlink"/></param>
+        static void SetCommand(winrt::Hyperlink const& obj, winrt::ICommand const& value);
+
+        /// <summary>
+        /// Gets the <see cref="CommandProperty"/> instance associated with the specified <see cref="Hyperlink"/>
+        /// </summary>
+        /// <param name="obj">The <see cref="Hyperlink"/> from which to get the associated <see cref="CommandParameterProperty"/> value</param>
+        /// <returns>The <see cref="CommandParameterProperty"/> value associated with the <see cref="Hyperlink"/> or null</returns>
+        static winrt::IInspectable GetCommandParameter(winrt::Hyperlink const& obj);
+
+        /// <summary>
+        /// Sets the <see cref="CommandProperty"/> associated with the specified <see cref="Hyperlink"/>
+        /// </summary>
+        /// <param name="obj">The <see cref="Hyperlink"/> to associated the <see cref="CommandParameterProperty"/> instance to</param>
+        /// <param name="value">The <see cref="object"/> to set the <see cref="CommandParameterProperty"/> to</param>
+        static void SetCommandParameter(winrt::Hyperlink const& obj, winrt::IInspectable const& value);
+
+    private:
+        static void OnCommandPropertyChanged(winrt::DependencyObject const& sender, winrt::DependencyPropertyChangedEventArgs const& args);
+
+        static void OnHyperlinkClicked(winrt::Hyperlink const& sender, winrt::HyperlinkClickEventArgs const& args);
+
+        static const wil::single_threaded_property<winrt::DependencyProperty> ClickEventTokenProperty;
+    };
+}
+
+namespace winrt::XamlToolkit::WinUI::factory_implementation
+{
+    struct HyperlinkExtensions : HyperlinkExtensionsT<HyperlinkExtensions, implementation::HyperlinkExtensions>
+    {
+    };
+}
