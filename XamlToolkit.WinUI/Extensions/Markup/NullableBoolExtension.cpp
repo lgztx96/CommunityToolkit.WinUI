b77ebaf16c9cd4c0ba27a23a90c315acf59d37e8
lgztx <z2337878191@outlook.com>
2026-06-16 01:05:48 +0800

    Add NullableBoolExtension markup extension

---
+++ XamlToolkit.WinUI/Extensions/Markup/NullableBoolExtension.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "winrt_module_imports.h"
+#include "NullableBoolExtension.h"
+#if __has_include("NullableBoolExtension.g.cpp")
+#include "NullableBoolExtension.g.cpp"
+#endif
+
+namespace winrt::XamlToolkit::WinUI::implementation
+{
+	winrt::IInspectable NullableBoolExtension::ProvideValue() const
+	{
+		if (IsNull())
+		{
+			return nullptr;
+		}
+
+		return winrt::box_value(Value());
+	}
+
+	winrt::IInspectable NullableBoolExtension::ProvideValue([[maybe_unused]] winrt::IXamlServiceProvider const& provider) const
+	{
+		return ProvideValue();
+	}
+}
---
+++ XamlToolkit.WinUI/Extensions/Markup/NullableBoolExtension.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "NullableBoolExtension.g.h"
+
+namespace winrt
+{
+    using namespace Windows::Foundation;
+    using namespace Microsoft::UI::Xaml;
+}
+
+namespace winrt::XamlToolkit::WinUI::implementation
+{
+    struct NullableBoolExtension : NullableBoolExtensionT<NullableBoolExtension>
+    {
+        NullableBoolExtension() = default;
+
+        wil::single_threaded_rw_property<bool> Value{ false };
+
+        wil::single_threaded_rw_property<bool> IsNull{ false };
+
+		winrt::IInspectable ProvideValue() const;
+
+		winrt::IInspectable ProvideValue(winrt::IXamlServiceProvider const& provider) const;
+    };
+}
+
+namespace winrt::XamlToolkit::WinUI::factory_implementation
+{
+    struct NullableBoolExtension : NullableBoolExtensionT<NullableBoolExtension, implementation::NullableBoolExtension>
+    {
+    };
+}
