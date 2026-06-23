a2b5e8a97b3c88212bcc8c743b0b0d8238eb9ac0
lgztx <z2337878191@outlook.com>
2026-06-23 22:49:17 +0800

    [XamlToolkit.WinUI] Clean up ControlSizeTrigger dependency properties

--- XamlToolkit.WinUI/Triggers/ControlSizeTrigger.cpp
+++ XamlToolkit.WinUI/Triggers/ControlSizeTrigger.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "winrt_module_imports.h"
 #include "ControlSizeTrigger.h"
 #if __has_include("ControlSizeTrigger.g.cpp")
@@ -7,20 +7,74 @@
 
 namespace winrt::XamlToolkit::WinUI::implementation
 {
+	const wil::single_threaded_property<winrt::DependencyProperty> ControlSizeTrigger::CanTriggerProperty =
+		winrt::DependencyProperty::Register(
+			L"CanTrigger",
+			winrt::xaml_typename<bool>(),
+			winrt::xaml_typename<class_type>(),
+			winrt::PropertyMetadata(winrt::box_value(true), &ControlSizeTrigger::OnSizeTriggerPropertyChanged));
+
+	const wil::single_threaded_property<winrt::DependencyProperty> ControlSizeTrigger::MaxWidthProperty =
+		winrt::DependencyProperty::Register(
+			L"MaxWidth",
+			winrt::xaml_typename<double>(),
+			winrt::xaml_typename<class_type>(),
+			winrt::PropertyMetadata(winrt::box_value(std::numeric_limits<double>::infinity()), &ControlSizeTrigger::OnSizeTriggerPropertyChanged));
+
+	const wil::single_threaded_property<winrt::DependencyProperty> ControlSizeTrigger::MinWidthProperty =
+		winrt::DependencyProperty::Register(
+			L"MinWidth",
+			winrt::xaml_typename<double>(),
+			winrt::xaml_typename<class_type>(),
+			winrt::PropertyMetadata(winrt::box_value(0.0), &ControlSizeTrigger::OnSizeTriggerPropertyChanged));
+
+	const wil::single_threaded_property<winrt::DependencyProperty> ControlSizeTrigger::MaxHeightProperty =
+		winrt::DependencyProperty::Register(
+			L"MaxHeight",
+			winrt::xaml_typename<double>(),
+			winrt::xaml_typename<class_type>(),
+			winrt::PropertyMetadata(winrt::box_value(std::numeric_limits<double>::infinity()), &ControlSizeTrigger::OnSizeTriggerPropertyChanged));
+
+	const wil::single_threaded_property<winrt::DependencyProperty> ControlSizeTrigger::MinHeightProperty =
+		winrt::DependencyProperty::Register(
+			L"MinHeight",
+			winrt::xaml_typename<double>(),
+			winrt::xaml_typename<class_type>(),
+			winrt::PropertyMetadata(winrt::box_value(0.0), &ControlSizeTrigger::OnSizeTriggerPropertyChanged));
+
+	const wil::single_threaded_property<winrt::DependencyProperty> ControlSizeTrigger::TargetElementProperty =
+		winrt::DependencyProperty::Register(
+			L"TargetElement",
+			winrt::xaml_typename<winrt::FrameworkElement>(),
+			winrt::xaml_typename<class_type>(),
+			winrt::PropertyMetadata(nullptr, &ControlSizeTrigger::OnTargetElementPropertyChanged));
+
+	void ControlSizeTrigger::OnTargetElementPropertyChanged(winrt::DependencyObject const& d, winrt::DependencyPropertyChangedEventArgs const& e)
+	{
+		auto self = winrt::get_self<ControlSizeTrigger>(d.as<class_type>())->get_strong();
+		self->UpdateTargetElement(e.OldValue().as<winrt::FrameworkElement>(), e.NewValue().as<winrt::FrameworkElement>());
+	}
+
+	void ControlSizeTrigger::OnSizeTriggerPropertyChanged(winrt::DependencyObject const& d, winrt::DependencyPropertyChangedEventArgs const&)
+	{
+		auto self = winrt::get_self<ControlSizeTrigger>(d.as<class_type>())->get_strong();
+		self->UpdateTrigger();
+	}
+
 	// Handle event to get current values
-	void ControlSizeTrigger::OnTargetElementSizeChanged([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] SizeChangedEventArgs const& e)
+	void ControlSizeTrigger::OnTargetElementSizeChanged([[maybe_unused]] winrt::IInspectable const& sender, [[maybe_unused]] winrt::SizeChangedEventArgs const& e)
 	{
 		UpdateTrigger();
 	}
 
-	void ControlSizeTrigger::UpdateTargetElement(FrameworkElement const& oldValue, FrameworkElement const& newValue)
+	void ControlSizeTrigger::UpdateTargetElement(winrt::FrameworkElement const& oldValue, winrt::FrameworkElement const& newValue)
 	{
-		if (oldValue != nullptr)
+		if (oldValue)
 		{
 			_sizeChangedRevoker.revoke();
 		}
 
-		if (newValue != nullptr)
+		if (newValue)
 		{
 			_sizeChangedRevoker = newValue.SizeChanged(winrt::auto_revoke, { this, &ControlSizeTrigger::OnTargetElementSizeChanged });
 		}
@@ -31,16 +85,17 @@ namespace winrt::XamlToolkit::WinUI::implementation
 	// Logic to evaluate and apply trigger value
 	void ControlSizeTrigger::UpdateTrigger()
 	{
-		if (TargetElement() == nullptr || !CanTrigger())
+		auto targetElement = TargetElement();
+		if (targetElement == nullptr || !CanTrigger())
 		{
 			SetActive(false);
 			return;
 		}
 
-		bool activate = MinWidth() <= TargetElement().ActualWidth() &&
-			TargetElement().ActualWidth() < MaxWidth() &&
-			MinHeight() <= TargetElement().ActualHeight() &&
-			TargetElement().ActualHeight() < MaxHeight();
+		bool activate = MinWidth() <= targetElement.ActualWidth() &&
+			targetElement.ActualWidth() < MaxWidth() &&
+			MinHeight() <= targetElement.ActualHeight() &&
+			targetElement.ActualHeight() < MaxHeight();
 
 		IsActive = activate;
 		SetActive(activate);
--- XamlToolkit.WinUI/Triggers/ControlSizeTrigger.h
+++ XamlToolkit.WinUI/Triggers/ControlSizeTrigger.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include "ControlSizeTrigger.g.h"
 
@@ -12,10 +12,10 @@
 
 namespace winrt
 {
+    using namespace Windows::Foundation;
     using namespace Microsoft::UI::Xaml;
     using namespace Microsoft::UI::Xaml::Controls;
     using namespace Microsoft::UI::Xaml::Controls::Primitives;
-    using namespace Microsoft::UI::Xaml::Input;
 }
 
 namespace winrt::XamlToolkit::WinUI::implementation
@@ -24,97 +24,52 @@ namespace winrt::XamlToolkit::WinUI::implementation
     {
         ControlSizeTrigger() = default;
 
-        FrameworkElement::SizeChanged_revoker _sizeChangedRevoker;
-
         bool CanTrigger() const { return winrt::unbox_value<bool>(GetValue(CanTriggerProperty)); }
         void CanTrigger(bool value) const { SetValue(CanTriggerProperty, winrt::box_value(value)); }
 
-        static inline const wil::single_threaded_property<DependencyProperty> CanTriggerProperty = DependencyProperty::Register(
-            L"CanTrigger",
-            winrt::xaml_typename<bool>(),
-            winrt::xaml_typename<class_type>(),
-            PropertyMetadata(winrt::box_value(true), [](auto&& d, auto&&) 
-                { 
-                    auto self = winrt::get_self<ControlSizeTrigger>(d.template as<class_type>())->get_strong();
-                    self->UpdateTrigger();
-                }));
+        static const wil::single_threaded_property<winrt::DependencyProperty> CanTriggerProperty;
 
         double MaxWidth() const { return winrt::unbox_value<double>(GetValue(MaxWidthProperty)); }
         void MaxWidth(double value) const { SetValue(MaxWidthProperty, winrt::box_value(value)); }
 
-        static inline const wil::single_threaded_property<DependencyProperty> MaxWidthProperty = DependencyProperty::Register(
-            L"MaxWidth",
-            winrt::xaml_typename<double>(),
-            winrt::xaml_typename<class_type>(),
-            PropertyMetadata(winrt::box_value(std::numeric_limits<double>::infinity()), [](auto&& d, auto&&) 
-                {
-                    auto self = winrt::get_self<ControlSizeTrigger>(d.template as<class_type>())->get_strong();
-                    self->UpdateTrigger();
-                }));
+        static const wil::single_threaded_property<winrt::DependencyProperty> MaxWidthProperty;
 
         double MinWidth() const { return winrt::unbox_value<double>(GetValue(MinWidthProperty)); }
         void MinWidth(double value) const { SetValue(MinWidthProperty, winrt::box_value(value)); }
 
-        static inline const wil::single_threaded_property<DependencyProperty> MinWidthProperty = DependencyProperty::Register(
-            L"MinWidth",
-            winrt::xaml_typename<double>(),
-            winrt::xaml_typename<class_type>(),
-            PropertyMetadata(winrt::box_value(0.0), [](auto&& d, auto&&)
-                { 
-                    auto self = winrt::get_self<ControlSizeTrigger>(d.template as<class_type>())->get_strong();
-                    self->UpdateTrigger();
-                }));
+        static const wil::single_threaded_property<winrt::DependencyProperty> MinWidthProperty;
 
         double MaxHeight() const { return winrt::unbox_value<double>(GetValue(MaxHeightProperty)); }
         void MaxHeight(double value) const { SetValue(MaxHeightProperty, winrt::box_value(value)); }
 
-        static inline const wil::single_threaded_property<DependencyProperty> MaxHeightProperty = DependencyProperty::Register(
-            L"MaxHeight",
-            winrt::xaml_typename<double>(),
-            winrt::xaml_typename<class_type>(),
-            PropertyMetadata(winrt::box_value(std::numeric_limits<double>::infinity()), [](auto&& d, auto&&)
-                { 
-                    auto self = winrt::get_self<ControlSizeTrigger>(d.template as<class_type>())->get_strong();
-                    self->UpdateTrigger();
-                }));
+        static const wil::single_threaded_property<winrt::DependencyProperty> MaxHeightProperty;
 
         double MinHeight() const { return winrt::unbox_value<double>(GetValue(MinHeightProperty)); }
         void MinHeight(double value) const { SetValue(MinHeightProperty, winrt::box_value(value)); }
 
-        static inline const wil::single_threaded_property<DependencyProperty> MinHeightProperty = DependencyProperty::Register(
-            L"MinHeight",
-            winrt::xaml_typename<double>(),
-            winrt::xaml_typename <class_type>(),
-            PropertyMetadata(winrt::box_value(0.0), [](auto&& d, auto&&)
-                { 
-                    auto self = winrt::get_self<ControlSizeTrigger>(d.template as<class_type>())->get_strong();
-                    self->UpdateTrigger();
-                }));
+        static const wil::single_threaded_property<winrt::DependencyProperty> MinHeightProperty;
 
-        FrameworkElement TargetElement() const { return winrt::unbox_value<FrameworkElement>(GetValue(TargetElementProperty)); }
-        void TargetElement(FrameworkElement const& value) const { SetValue(TargetElementProperty, winrt::box_value(value)); }
+        winrt::FrameworkElement TargetElement() const { return winrt::unbox_value<winrt::FrameworkElement>(GetValue(TargetElementProperty)); }
+        void TargetElement(winrt::FrameworkElement const& value) const { SetValue(TargetElementProperty, winrt::box_value(value)); }
 
-        static void OnTargetElementPropertyChanged(DependencyObject const& d, DependencyPropertyChangedEventArgs const& e)
-        {
-            winrt::get_self<ControlSizeTrigger>(d.as<class_type>())->
-                UpdateTargetElement(e.OldValue().as<FrameworkElement>(), e.NewValue().as<FrameworkElement>());
-        }
-
-        static inline const wil::single_threaded_property<DependencyProperty> TargetElementProperty = DependencyProperty::Register(
-                L"TargetElement",
-                winrt::xaml_typename<FrameworkElement>(),
-                winrt::xaml_typename<class_type>(),
-                PropertyMetadata(nullptr, &ControlSizeTrigger::OnTargetElementPropertyChanged));
+        static const wil::single_threaded_property<winrt::DependencyProperty> TargetElementProperty;
 
         wil::single_threaded_property<bool> IsActive;
 
         // Handle event to get current values
-        void OnTargetElementSizeChanged(IInspectable const& sender, SizeChangedEventArgs const& e);
+        void OnTargetElementSizeChanged(winrt::IInspectable const& sender, winrt::SizeChangedEventArgs const& e);
 
-        void UpdateTargetElement(FrameworkElement const& oldValue, FrameworkElement const& newValue);
+        void UpdateTargetElement(winrt::FrameworkElement const& oldValue, winrt::FrameworkElement const& newValue);
 
         // Logic to evaluate and apply trigger value
         void UpdateTrigger();
+
+    private:
+        static void OnTargetElementPropertyChanged(winrt::DependencyObject const& d, winrt::DependencyPropertyChangedEventArgs const& e);
+
+        static void OnSizeTriggerPropertyChanged(winrt::DependencyObject const& d, winrt::DependencyPropertyChangedEventArgs const&);
+
+        winrt::FrameworkElement::SizeChanged_revoker _sizeChangedRevoker;
     };
 }
 
