2c17186cfee759ac4d1ea827c22b24ca7c5d7ef5
lgztx96 <z2337878191@outlook.com>
2026-06-23 14:48:20 +0800

    [XamlToolkit.WinUI] Add UserHandPreferenceStateTrigger

---
+++ XamlToolkit.WinUI/Triggers/UserHandPreferenceStateTrigger.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "winrt_module_imports.h"
+#include "UserHandPreferenceStateTrigger.h"
+#if __has_include("UserHandPreferenceStateTrigger.g.cpp")
+#include "UserHandPreferenceStateTrigger.g.cpp"
+#endif
+
+namespace winrt::XamlToolkit::WinUI::implementation
+{
+    const wil::single_threaded_property<winrt::DependencyProperty> UserHandPreferenceStateTrigger::HandPreferenceProperty =
+        winrt::DependencyProperty::Register(
+            L"HandPreference",
+            winrt::xaml_typename<winrt::HandPreference>(),
+            winrt::xaml_typename<class_type>(),
+            winrt::PropertyMetadata(winrt::box_value(winrt::HandPreference::RightHanded), &UserHandPreferenceStateTrigger::OnHandPreferencePropertyChanged));
+
+    UserHandPreferenceStateTrigger::UserHandPreferenceStateTrigger()
+    {
+        winrt::UISettings settings;
+        _handPreference = settings.HandPreference();
+    }
+
+    void UserHandPreferenceStateTrigger::OnHandPreferencePropertyChanged(winrt::DependencyObject const& d, winrt::DependencyPropertyChangedEventArgs const& e)
+    {
+        auto obj = d.as<class_type>();
+        auto self = winrt::get_self<UserHandPreferenceStateTrigger>(obj);
+        auto val = winrt::unbox_value<winrt::HandPreference>(e.NewValue());
+
+        self->SetActive(self->_handPreference == val);
+    }
+}
---
+++ XamlToolkit.WinUI/Triggers/UserHandPreferenceStateTrigger.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "UserHandPreferenceStateTrigger.g.h"
+
+#ifdef __INTELLISENSE__
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Windows.UI.ViewManagement.h>
+#endif
+
+namespace winrt
+{
+    using namespace Microsoft::UI::Xaml;
+    using namespace Windows::UI::ViewManagement;
+}
+
+namespace winrt::XamlToolkit::WinUI::implementation
+{
+    struct UserHandPreferenceStateTrigger : UserHandPreferenceStateTriggerT<UserHandPreferenceStateTrigger>
+    {
+        UserHandPreferenceStateTrigger();
+
+        winrt::HandPreference HandPreference() const { return winrt::unbox_value<winrt::HandPreference>(GetValue(HandPreferenceProperty)); }
+        void HandPreference(winrt::HandPreference const& value) const { SetValue(HandPreferenceProperty, winrt::box_value(value)); }
+
+        static const wil::single_threaded_property<winrt::DependencyProperty> HandPreferenceProperty;
+
+    private:
+        static void OnHandPreferencePropertyChanged(winrt::DependencyObject const& d, winrt::DependencyPropertyChangedEventArgs const& e);
+
+        winrt::HandPreference _handPreference;
+    };
+}
+
+namespace winrt::XamlToolkit::WinUI::factory_implementation
+{
+    struct UserHandPreferenceStateTrigger : UserHandPreferenceStateTriggerT<UserHandPreferenceStateTrigger, implementation::UserHandPreferenceStateTrigger>
+    {
+    };
+}
