06fd2b5a02889fa3a93cfa9ae3dd7f17517146b1
lgztx <z2337878191@outlook.com>
2026-07-03 00:00:57 +0800

    [SizerBase] Code cleanup

--- XamlToolkit.WinUI.Controls/Sizers/SizerBase.cpp
+++ XamlToolkit.WinUI.Controls/Sizers/SizerBase.cpp
@@ -10,6 +10,41 @@
 
 namespace winrt::XamlToolkit::WinUI::Controls::implementation
 {
+    const wil::single_threaded_property<winrt::DependencyProperty> SizerBase::CursorProperty =
+        winrt::DependencyProperty::Register(
+            L"Cursor",
+            winrt::xaml_typename<CursorEnum>(),
+            winrt::xaml_typename<class_type>(),
+            winrt::PropertyMetadata(nullptr, &SizerBase::OnOrientationPropertyChanged));
+
+    const wil::single_threaded_property<winrt::DependencyProperty> SizerBase::DragIncrementProperty =
+        winrt::DependencyProperty::Register(
+            L"DragIncrement",
+            winrt::xaml_typename<double>(),
+            winrt::xaml_typename<class_type>(),
+            winrt::PropertyMetadata(winrt::box_value(1.0)));
+
+    const wil::single_threaded_property<winrt::DependencyProperty> SizerBase::KeyboardIncrementProperty =
+        winrt::DependencyProperty::Register(
+            L"KeyboardIncrement",
+            winrt::xaml_typename<double>(),
+            winrt::xaml_typename<class_type>(),
+            winrt::PropertyMetadata(winrt::box_value(8.0)));
+
+    const wil::single_threaded_property<winrt::DependencyProperty> SizerBase::OrientationProperty =
+        winrt::DependencyProperty::Register(
+            L"Orientation",
+            winrt::xaml_typename<winrt::Orientation>(),
+            winrt::xaml_typename<class_type>(),
+            winrt::PropertyMetadata(winrt::box_value(winrt::Orientation::Vertical), &SizerBase::OnOrientationPropertyChanged));
+
+    const wil::single_threaded_property<winrt::DependencyProperty> SizerBase::IsThumbVisibleProperty =
+        winrt::DependencyProperty::Register(
+            L"IsThumbVisible",
+            winrt::xaml_typename<bool>(),
+            winrt::xaml_typename<class_type>(),
+            winrt::PropertyMetadata(winrt::box_value(true), &SizerBase::OnIsThumbVisiblePropertyChanged));
+
     SizerBase::SizerBase()
     {
         DefaultStyleKey(winrt::box_value(winrt::xaml_typename<class_type>()));
@@ -38,54 +73,54 @@ namespace winrt::XamlToolkit::WinUI::Controls::implementation
         // Ensure we have the proper cursor value setup, as we can only set now for WinUI 3
         OnOrientationPropertyChanged(*this, nullptr);
 
-        // Ensure we set the Thumb visiblity
+        // Ensure we set the Thumb visibility
         OnIsThumbVisiblePropertyChanged(*this, nullptr);
     }
 
-    void SizerBase::SizerBase_Loaded([[maybe_unused]] IInspectable const& sender, RoutedEventArgs const& e)
+    void SizerBase::SizerBase_Loaded([[maybe_unused]] winrt::IInspectable const& sender, winrt::RoutedEventArgs const& e)
     {
         _loadedRevoker.revoke();
 
         overridable().OnLoaded(e);
     }
 
-    void SizerBase::OnOrientationPropertyChanged(DependencyObject const& d, [[maybe_unused]] DependencyPropertyChangedEventArgs const& e)
+    void SizerBase::OnOrientationPropertyChanged(winrt::DependencyObject const& d, [[maybe_unused]] winrt::DependencyPropertyChangedEventArgs const& e)
     {
         if (auto sizerBase = d.try_as<class_type>())
         {
             auto gripper = winrt::get_self<SizerBase>(sizerBase)->get_strong();
-            VisualStateManager::GoToState(sizerBase, gripper->Orientation() == Orientation::Vertical ? VerticalState : HorizontalState, true);
+            winrt::VisualStateManager::GoToState(sizerBase, gripper->Orientation() == winrt::Orientation::Vertical ? VerticalState : HorizontalState, true);
 
-            CursorEnum cursorByOrientation = gripper->Orientation() == Orientation::Vertical ? CursorEnum::SizeWestEast : CursorEnum::SizeNorthSouth;
+            auto cursorByOrientation = gripper->Orientation() == winrt::Orientation::Vertical ? CursorEnum::SizeWestEast : CursorEnum::SizeNorthSouth;
 
             // See if there's been a cursor override, otherwise we'll pick
             auto cursor = gripper->ReadLocalValue(CursorProperty);
-            if (cursor == DependencyProperty::UnsetValue() || cursor == nullptr)
+            if (cursor == winrt::DependencyProperty::UnsetValue() || cursor == nullptr)
             {
                 cursor = winrt::box_value(cursorByOrientation);
             }
 
             // Need to wait until we're at least applying template step of loading before setting Cursor
             // See https://github.com/microsoft/microsoft-ui-xaml/issues/7062
-            auto cursorValue = cursor.try_as<CursorEnum>();
-            auto current = gripper->ProtectedCursor().try_as<Microsoft::UI::Input::InputSystemCursor>();
+            const auto cursorValue = cursor.try_as<CursorEnum>();
+            const auto current = gripper->ProtectedCursor().try_as<winrt::Microsoft::UI::Input::InputSystemCursor>();
             if (gripper->_appliedTemplate && cursorValue &&
                 (!current || current.CursorShape() != cursorValue))
             {
-                gripper->ProtectedCursor(Microsoft::UI::Input::InputSystemCursor::Create(*cursorValue));
+                gripper->ProtectedCursor(winrt::Microsoft::UI::Input::InputSystemCursor::Create(*cursorValue));
             }
         }
     }
 
-    void SizerBase::OnIsThumbVisiblePropertyChanged(DependencyObject const& d, [[maybe_unused]] DependencyPropertyChangedEventArgs const& e)
+    void SizerBase::OnIsThumbVisiblePropertyChanged(winrt::DependencyObject const& d, [[maybe_unused]] winrt::DependencyPropertyChangedEventArgs const& e)
     {
         if (auto gripper = d.try_as<class_type>())
         {
-            VisualStateManager::GoToState(gripper, gripper.IsThumbVisible() ? VisibleState : CollapsedState, true);
+            winrt::VisualStateManager::GoToState(gripper, gripper.IsThumbVisible() ? VisibleState : CollapsedState, true);
         }
     }
 
-    void SizerBase::OnKeyDown(KeyRoutedEventArgs const& e)
+    void SizerBase::OnKeyDown(winrt::KeyRoutedEventArgs const& e)
     {
         // If we're manipulating with mouse/touch, we ignore keyboard inputs.
         if (_dragging)
@@ -103,68 +138,69 @@ namespace winrt::XamlToolkit::WinUI::Controls::implementation
 
         overridable().OnDragStarting();
 
-        if (Orientation() == Orientation::Vertical)
+        if (Orientation() == winrt::Orientation::Vertical)
         {
             auto horizontalChange = KeyboardIncrement();
 
-            if (FlowDirection() == FlowDirection::RightToLeft)
+            if (FlowDirection() == winrt::FlowDirection::RightToLeft)
             {
                 horizontalChange *= -1;
             }
 
-            if (e.Key() == Windows::System::VirtualKey::Left)
+            if (e.Key() == winrt::Windows::System::VirtualKey::Left)
             {
                 OnDragHorizontal(-horizontalChange);
             }
-            else if (e.Key() == Windows::System::VirtualKey::Right)
+            else if (e.Key() == winrt::Windows::System::VirtualKey::Right)
             {
                 OnDragHorizontal(horizontalChange);
             }
         }
         else
         {
-            if (e.Key() == Windows::System::VirtualKey::Up)
+            if (e.Key() == winrt::Windows::System::VirtualKey::Up)
             {
                 overridable().OnDragVertical(-KeyboardIncrement());
             }
-            else if (e.Key() == Windows::System::VirtualKey::Down)
+            else if (e.Key() == winrt::Windows::System::VirtualKey::Down)
             {
                 overridable().OnDragVertical(KeyboardIncrement());
             }
         }
     }
 
-    void SizerBase::OnManipulationStarting(ManipulationStartingRoutedEventArgs const& e)
+    void SizerBase::OnManipulationStarting(winrt::ManipulationStartingRoutedEventArgs const& e)
     {
         base_type::OnManipulationStarting(e);
 
         overridable().OnDragStarting();
     }
 
-    void SizerBase::OnManipulationDelta(ManipulationDeltaRoutedEventArgs const& e)
+    void SizerBase::OnManipulationDelta(winrt::ManipulationDeltaRoutedEventArgs const& e)
     {
         // We use Truncate here to provide 'snapping' points with the DragIncrement property
         // It works for both our negative and positive values, as otherwise we'd need to use
         // Ceiling when negative and Floor when positive to maintain the correct behavior.
-        auto horizontalChange =
-            std::trunc(e.Cumulative().Translation.X / DragIncrement()) * DragIncrement();
-        auto verticalChange =
-            std::trunc(e.Cumulative().Translation.Y / DragIncrement()) * DragIncrement();
+		const auto dragIncrement = DragIncrement();
+		const auto cumulativeTranslation = e.Cumulative().Translation;
+
+        auto horizontalChange = std::trunc(cumulativeTranslation.X / dragIncrement) * dragIncrement;
+        auto verticalChange = std::trunc(cumulativeTranslation.Y / dragIncrement) * dragIncrement;
 
         // Important: adjust for RTL language flow settings and invert horizontal axis
-        if (FlowDirection() == FlowDirection::RightToLeft)
+        if (FlowDirection() == winrt::FlowDirection::RightToLeft)
         {
             horizontalChange *= -1;
         }
 
-        if (Orientation() == Orientation::Vertical)
+        if (Orientation() == winrt::Orientation::Vertical)
         {
             if (!overridable().OnDragHorizontal(horizontalChange))
             {
                 return;
             }
         }
-        else if (Orientation() == Orientation::Horizontal)
+        else if (Orientation() == winrt::Orientation::Horizontal)
         {
             if (!overridable().OnDragVertical(verticalChange))
             {
@@ -175,72 +211,72 @@ namespace winrt::XamlToolkit::WinUI::Controls::implementation
         base_type::OnManipulationDelta(e);
     }
 
-    void SizerBase::SizerBase_PointerReleased([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] PointerRoutedEventArgs const& e)
+    void SizerBase::SizerBase_PointerReleased([[maybe_unused]] winrt::IInspectable const& sender, [[maybe_unused]] winrt::PointerRoutedEventArgs const& e)
     {
         _pressed = false;
 
         if (IsEnabled())
         {
-            VisualStateManager::GoToState(*this, _pointerEntered ? PointerOverState : NormalState, true);
+            winrt::VisualStateManager::GoToState(*this, _pointerEntered ? PointerOverState : NormalState, true);
         }
     }
 
-    void SizerBase::SizerBase_PointerPressed([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] PointerRoutedEventArgs const& e)
+    void SizerBase::SizerBase_PointerPressed([[maybe_unused]] winrt::IInspectable const& sender, [[maybe_unused]] winrt::PointerRoutedEventArgs const& e)
     {
         _pressed = true;
 
         if (IsEnabled())
         {
-            VisualStateManager::GoToState(*this, PointerOverState, true);
+            winrt::VisualStateManager::GoToState(*this, PointerOverState, true);
         }
     }
 
-    void SizerBase::SizerBase_PointerExited([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] PointerRoutedEventArgs const& e)
+    void SizerBase::SizerBase_PointerExited([[maybe_unused]] winrt::IInspectable const& sender, [[maybe_unused]] winrt::PointerRoutedEventArgs const& e)
     {
         _pointerEntered = false;
 
         if (!_pressed && !_dragging && IsEnabled())
         {
-            VisualStateManager::GoToState(*this, NormalState, true);
+            winrt::VisualStateManager::GoToState(*this, NormalState, true);
         }
     }
 
-    void SizerBase::SizerBase_PointerEntered([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] PointerRoutedEventArgs const& e)
+    void SizerBase::SizerBase_PointerEntered([[maybe_unused]] winrt::IInspectable const& sender, [[maybe_unused]] winrt::PointerRoutedEventArgs const& e)
     {
         _pointerEntered = true;
 
         if (!_pressed && !_dragging && IsEnabled())
         {
-            VisualStateManager::GoToState(*this, PointerOverState, true);
+            winrt::VisualStateManager::GoToState(*this, PointerOverState, true);
         }
     }
 
-    void SizerBase::SizerBase_ManipulationCompleted([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] ManipulationCompletedRoutedEventArgs const& e)
+    void SizerBase::SizerBase_ManipulationCompleted([[maybe_unused]] winrt::IInspectable const& sender, [[maybe_unused]] winrt::ManipulationCompletedRoutedEventArgs const& e)
     {
         _dragging = false;
         _pressed = false;
-        VisualStateManager::GoToState(*this, _pointerEntered ? PointerOverState : NormalState, true);
+        winrt::VisualStateManager::GoToState(*this, _pointerEntered ? PointerOverState : NormalState, true);
     }
 
-    void SizerBase::SizerBase_ManipulationStarted([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] ManipulationStartedRoutedEventArgs const& e)
+    void SizerBase::SizerBase_ManipulationStarted([[maybe_unused]] winrt::IInspectable const& sender, [[maybe_unused]] winrt::ManipulationStartedRoutedEventArgs const& e)
     {
         _dragging = true;
-        VisualStateManager::GoToState(*this, PressedState, true);
+        winrt::VisualStateManager::GoToState(*this, PressedState, true);
     }
 
-    void SizerBase::SizerBase_IsEnabledChanged([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] DependencyPropertyChangedEventArgs const& e)
+    void SizerBase::SizerBase_IsEnabledChanged([[maybe_unused]] winrt::IInspectable const& sender, [[maybe_unused]] winrt::DependencyPropertyChangedEventArgs const& e)
     {
         if (!IsEnabled())
         {
-            VisualStateManager::GoToState(*this, DisabledState, true);
+            winrt::VisualStateManager::GoToState(*this, DisabledState, true);
         }
         else
         {
-            VisualStateManager::GoToState(*this, _pointerEntered ? PointerOverState : NormalState, true);
+            winrt::VisualStateManager::GoToState(*this, _pointerEntered ? PointerOverState : NormalState, true);
         }
     }
 
-    bool SizerBase::IsValidHeight(FrameworkElement const& target, double newHeight, double parentActualHeight)
+    bool SizerBase::IsValidHeight(winrt::FrameworkElement const& target, double newHeight, double parentActualHeight)
     {
         auto minHeight = target.MinHeight();
         if (newHeight < 0 || (!std::isnan(minHeight) && newHeight < minHeight))
@@ -262,7 +298,7 @@ namespace winrt::XamlToolkit::WinUI::Controls::implementation
         return true;
     }
 
-    bool SizerBase::IsValidWidth(FrameworkElement const& target, double newWidth, double parentActualWidth)
+    bool SizerBase::IsValidWidth(winrt::FrameworkElement const& target, double newWidth, double parentActualWidth)
     {
         auto minWidth = target.MinWidth();
         if (newWidth < 0 || (!std::isnan(minWidth) && newWidth < minWidth))
--- XamlToolkit.WinUI.Controls/Sizers/SizerBase.h
+++ XamlToolkit.WinUI.Controls/Sizers/SizerBase.h
@@ -3,6 +3,7 @@
 #include "SizerBase.g.h"
 
 #ifdef __INTELLISENSE__
+#include <winrt/Windows.Foundation.h>
 #include <winrt/Microsoft.UI.Input.h>
 #include <winrt/Microsoft.UI.Xaml.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
@@ -14,6 +15,7 @@
 
 namespace winrt
 {
+    using namespace Windows::Foundation;
     using namespace Microsoft::UI::Xaml;
     using namespace Microsoft::UI::Xaml::Controls;
     using namespace Microsoft::UI::Xaml::Controls::Primitives;
@@ -23,9 +25,6 @@ namespace winrt
 namespace winrt::XamlToolkit::WinUI::Controls::implementation
 {
     using CursorEnum = Microsoft::UI::Input::InputSystemCursorShape;
-
-    template <typename D, typename... I>
-    using SizerBaseT = SizerBase_base<D, I...>;
 
     struct SizerBase : SizerBaseT<SizerBase>
     {
@@ -41,14 +40,82 @@ namespace winrt::XamlToolkit::WinUI::Controls::implementation
         static constexpr auto VisibleState = L"Visible";
         static constexpr auto CollapsedState = L"Collapsed";
 
-        FrameworkElement::Loaded_revoker _loadedRevoker;
-        UIElement::PointerEntered_revoker _pointerEnteredRevoker;
-        UIElement::PointerExited_revoker _pointerExitedRevoker;
-        UIElement::PointerPressed_revoker _pointerPressedRevoker;
-        UIElement::PointerReleased_revoker _pointerReleasedRevoker;
-        UIElement::ManipulationStarted_revoker _manipulationStartedRevoker;
-        UIElement::ManipulationCompleted_revoker _manipulationCompletedRevoker;
-        Control::IsEnabledChanged_revoker _isEnabledChangedRevoker;
+        SizerBase();
+
+        virtual void OnLoaded([[maybe_unused]] winrt::RoutedEventArgs const& e) {}
+
+        virtual void OnDragStarting() {}
+
+        virtual bool OnDragHorizontal([[maybe_unused]] double horizontalChange) { return false; }
+
+        virtual bool OnDragVertical([[maybe_unused]] double verticalChange) { return false; }
+
+        void OnApplyTemplate();
+
+        void OnKeyDown(winrt::KeyRoutedEventArgs const& e);
+
+        void OnManipulationStarting(winrt::ManipulationStartingRoutedEventArgs const& e);
+
+        void OnManipulationDelta(winrt::ManipulationDeltaRoutedEventArgs const& e);
+
+        void SizerBase_Loaded(winrt::IInspectable const& sender, winrt::RoutedEventArgs const& e);
+
+        void SizerBase_PointerReleased([[maybe_unused]] winrt::IInspectable const& sender, winrt::PointerRoutedEventArgs const& e);
+
+        void SizerBase_PointerPressed([[maybe_unused]] winrt::IInspectable const& sender, winrt::PointerRoutedEventArgs const& e);
+
+        void SizerBase_PointerExited([[maybe_unused]] winrt::IInspectable const& sender, winrt::PointerRoutedEventArgs const& e);
+
+        void SizerBase_PointerEntered([[maybe_unused]] winrt::IInspectable const& sender, winrt::PointerRoutedEventArgs const& e);
+
+        void SizerBase_ManipulationCompleted([[maybe_unused]] winrt::IInspectable const& sender, winrt::ManipulationCompletedRoutedEventArgs const& e);
+
+        void SizerBase_ManipulationStarted([[maybe_unused]] winrt::IInspectable const& sender, winrt::ManipulationStartedRoutedEventArgs const& e);
+
+        void SizerBase_IsEnabledChanged([[maybe_unused]] winrt::IInspectable const& sender, winrt::DependencyPropertyChangedEventArgs const& e);
+
+        static bool IsValidHeight(winrt::FrameworkElement const& target, double newHeight, double parentActualHeight);
+
+        static bool IsValidWidth(winrt::FrameworkElement const& target, double newWidth, double parentActualWidth);
+
+        static void OnOrientationPropertyChanged(winrt::DependencyObject const& d, winrt::DependencyPropertyChangedEventArgs const& e);
+
+        static void OnIsThumbVisiblePropertyChanged(winrt::DependencyObject const& d, winrt::DependencyPropertyChangedEventArgs const& e);
+
+        CursorEnum Cursor() const { return winrt::unbox_value<CursorEnum>(GetValue(CursorProperty)); }
+        void Cursor(CursorEnum const& value) { SetValue(CursorProperty, winrt::box_value(value)); }
+
+        static const wil::single_threaded_property<winrt::DependencyProperty> CursorProperty;
+
+        double DragIncrement() const { return winrt::unbox_value<double>(GetValue(DragIncrementProperty)); }
+        void DragIncrement(double value) { SetValue(DragIncrementProperty, winrt::box_value(value)); }
+
+        static const wil::single_threaded_property<winrt::DependencyProperty> DragIncrementProperty;
+
+        double KeyboardIncrement() const { return winrt::unbox_value<double>(GetValue(KeyboardIncrementProperty)); }
+        void KeyboardIncrement(double value) { SetValue(KeyboardIncrementProperty, winrt::box_value(value)); }
+
+        static const wil::single_threaded_property<winrt::DependencyProperty> KeyboardIncrementProperty;
+
+        winrt::Orientation Orientation() const { return winrt::unbox_value<winrt::Orientation>(GetValue(OrientationProperty)); }
+        void Orientation(winrt::Orientation const& value) { SetValue(OrientationProperty, winrt::box_value(value)); }
+
+        static const wil::single_threaded_property<winrt::DependencyProperty> OrientationProperty;
+
+        bool IsThumbVisible() const { return winrt::unbox_value<bool>(GetValue(IsThumbVisibleProperty)); }
+        void IsThumbVisible(bool value) { SetValue(IsThumbVisibleProperty, winrt::box_value(value)); }
+
+        static const wil::single_threaded_property<winrt::DependencyProperty> IsThumbVisibleProperty;
+
+    private:
+        winrt::FrameworkElement::Loaded_revoker _loadedRevoker;
+        winrt::UIElement::PointerEntered_revoker _pointerEnteredRevoker;
+        winrt::UIElement::PointerExited_revoker _pointerExitedRevoker;
+        winrt::UIElement::PointerPressed_revoker _pointerPressedRevoker;
+        winrt::UIElement::PointerReleased_revoker _pointerReleasedRevoker;
+        winrt::UIElement::ManipulationStarted_revoker _manipulationStartedRevoker;
+        winrt::UIElement::ManipulationCompleted_revoker _manipulationCompletedRevoker;
+        winrt::Control::IsEnabledChanged_revoker _isEnabledChangedRevoker;
 
         bool _appliedTemplate = false;
 
@@ -56,76 +123,6 @@ namespace winrt::XamlToolkit::WinUI::Controls::implementation
         bool _pressed = false;
         bool _dragging = false;
         bool _pointerEntered = false;
-
-        virtual void OnLoaded([[maybe_unused]] RoutedEventArgs const& e) {}
-        virtual void OnDragStarting() {}
-        virtual bool OnDragHorizontal([[maybe_unused]] double horizontalChange) { return false; }
-        virtual bool OnDragVertical([[maybe_unused]] double verticalChange) { return false; }
-
-        SizerBase();
-
-        void OnApplyTemplate();
-
-        void OnKeyDown(KeyRoutedEventArgs const& e);
-
-        void OnManipulationStarting(ManipulationStartingRoutedEventArgs const& e);
-
-        void OnManipulationDelta(ManipulationDeltaRoutedEventArgs const& e);
-
-        void SizerBase_Loaded(IInspectable const& sender, RoutedEventArgs const& e);
-
-        void SizerBase_PointerReleased([[maybe_unused]] IInspectable const& sender, PointerRoutedEventArgs const& e);
-
-        void SizerBase_PointerPressed([[maybe_unused]] IInspectable const& sender, PointerRoutedEventArgs const& e);
-
-        void SizerBase_PointerExited([[maybe_unused]] IInspectable const& sender, PointerRoutedEventArgs const& e);
-
-        void SizerBase_PointerEntered([[maybe_unused]] IInspectable const& sender, PointerRoutedEventArgs const& e);
-
-        void SizerBase_ManipulationCompleted([[maybe_unused]] IInspectable const& sender, ManipulationCompletedRoutedEventArgs const& e);
-
-        void SizerBase_ManipulationStarted([[maybe_unused]] IInspectable const& sender, ManipulationStartedRoutedEventArgs const& e);
-
-        void SizerBase_IsEnabledChanged([[maybe_unused]] IInspectable const& sender, DependencyPropertyChangedEventArgs const& e);
-
-        static bool IsValidHeight(FrameworkElement const& target, double newHeight, double parentActualHeight);
-
-        static bool IsValidWidth(FrameworkElement const& target, double newWidth, double parentActualWidth);
-
-        static void OnOrientationPropertyChanged(DependencyObject const& d, DependencyPropertyChangedEventArgs const& e);
-
-        static void OnIsThumbVisiblePropertyChanged(DependencyObject const& d, DependencyPropertyChangedEventArgs const& e);
-
-        CursorEnum Cursor() { return winrt::unbox_value<CursorEnum>(GetValue(CursorProperty)); }
-        void Cursor(CursorEnum value) { SetValue(CursorProperty, winrt::box_value(value)); }
-
-        static inline const wil::single_threaded_property<DependencyProperty> CursorProperty =
-            DependencyProperty::Register(L"Cursor", winrt::xaml_typename <CursorEnum>(),
-                winrt::xaml_typename<class_type>(), PropertyMetadata(nullptr, OnOrientationPropertyChanged));
-
-        double DragIncrement() { return winrt::unbox_value<double>(GetValue(DragIncrementProperty)); }
-        void DragIncrement(double value) { SetValue(DragIncrementProperty, winrt::box_value(value)); }
-
-        static inline const wil::single_threaded_property<DependencyProperty> DragIncrementProperty =
-            DependencyProperty::Register(L"DragIncrement", winrt::xaml_typename<double>(), winrt::xaml_typename<class_type>(), PropertyMetadata(winrt::box_value(1.0)));
-
-        double KeyboardIncrement() { return winrt::unbox_value<double>(GetValue(KeyboardIncrementProperty)); }
-        void KeyboardIncrement(double value) { SetValue(KeyboardIncrementProperty, winrt::box_value(value)); }
-
-        static inline const wil::single_threaded_property<DependencyProperty> KeyboardIncrementProperty =
-            DependencyProperty::Register(L"KeyboardIncrement", winrt::xaml_typename<double>(), winrt::xaml_typename<class_type>(), PropertyMetadata(winrt::box_value(8.0)));
-
-        Orientation Orientation() { return winrt::unbox_value<Microsoft::UI::Xaml::Controls::Orientation>(GetValue(OrientationProperty)); }
-        void Orientation(Microsoft::UI::Xaml::Controls::Orientation value) { SetValue(OrientationProperty, winrt::box_value(value)); }
-
-        static inline const wil::single_threaded_property<DependencyProperty> OrientationProperty =
-            DependencyProperty::Register(L"Orientation", winrt::xaml_typename<Microsoft::UI::Xaml::Controls::Orientation>(), winrt::xaml_typename<class_type>(), PropertyMetadata(winrt::box_value(Orientation::Vertical), OnOrientationPropertyChanged));
-
-        bool IsThumbVisible() { return winrt::unbox_value<bool>(GetValue(IsThumbVisibleProperty)); }
-        void IsThumbVisible(bool value) { SetValue(IsThumbVisibleProperty, winrt::box_value(value)); }
-
-        static inline const wil::single_threaded_property<DependencyProperty> IsThumbVisibleProperty =
-            DependencyProperty::Register(L"IsThumbVisible", winrt::xaml_typename<bool>(), winrt::xaml_typename<class_type>(), PropertyMetadata(winrt::box_value(true), OnIsThumbVisiblePropertyChanged));
     };
 }
 
