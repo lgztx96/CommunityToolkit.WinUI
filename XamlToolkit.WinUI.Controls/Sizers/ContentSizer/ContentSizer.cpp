4257bb9cb842b79017f9f87e3623c88c75adba7d
lgztx96 <z2337878191@outlook.com>
2026-07-02 21:47:39 +0800

    [ContentSizer] Code cleanup

--- XamlToolkit.WinUI.Controls/Sizers/ContentSizer/ContentSizer.cpp
+++ XamlToolkit.WinUI.Controls/Sizers/ContentSizer/ContentSizer.cpp
@@ -8,65 +8,79 @@
 
 namespace winrt::XamlToolkit::WinUI::Controls::implementation
 {
+	const wil::single_threaded_property<winrt::DependencyProperty> ContentSizer::IsDragInvertedProperty =
+		winrt::DependencyProperty::Register(
+			L"IsDragInverted",
+			winrt::xaml_typename<bool>(),
+			winrt::xaml_typename<class_type>(),
+			winrt::PropertyMetadata(winrt::box_value(false)));
+
+	const wil::single_threaded_property<winrt::DependencyProperty> ContentSizer::TargetControlProperty =
+		winrt::DependencyProperty::Register(
+			L"TargetControl",
+			winrt::xaml_typename<winrt::FrameworkElement>(),
+			winrt::xaml_typename<class_type>(),
+			winrt::PropertyMetadata(nullptr));
+
 	ContentSizer::ContentSizer() : _currentSize(0.0)
 	{
 		DefaultStyleKey(winrt::box_value(winrt::xaml_typename<class_type>()));
 	}
 
-	void ContentSizer::OnLoaded([[maybe_unused]] RoutedEventArgs const& e)
+	void ContentSizer::OnLoaded([[maybe_unused]] winrt::RoutedEventArgs const& e)
 	{
 		if (TargetControl() == nullptr)
 		{
-			TargetControl(DependencyObjectEx::FindAscendant<FrameworkElement>(*this));
+			TargetControl(DependencyObjectEx::FindAscendant<winrt::FrameworkElement>(*this));
 		}
 	}
 
 	void ContentSizer::OnDragStarting()
 	{
-		if (TargetControl() != nullptr)
+		if (const auto targetControl = TargetControl())
 		{
 			_currentSize =
-				Orientation() == Orientation::Vertical ?
-				TargetControl().ActualWidth() :
-				TargetControl().ActualHeight();
+				Orientation() == winrt::Orientation::Vertical ?
+				targetControl.ActualWidth() :
+				targetControl.ActualHeight();
 		}
 	}
 
 	bool ContentSizer::OnDragHorizontal(double horizontalChange)
 	{
-		if (TargetControl() == nullptr)
+		if (const auto targetControl = TargetControl())
 		{
+			horizontalChange = IsDragInverted() ? -horizontalChange : horizontalChange;
+
+			if (!IsValidWidth(targetControl, _currentSize + horizontalChange, ActualWidth()))
+			{
+				return false;
+			}
+
+			targetControl.Width(_currentSize + horizontalChange);
+
 			return true;
 		}
 
-		horizontalChange = IsDragInverted() ? -horizontalChange : horizontalChange;
-
-		if (!IsValidWidth(TargetControl(), _currentSize + horizontalChange, ActualWidth()))
-		{
-			return false;
-		}
-
-		TargetControl().Width(_currentSize + horizontalChange);
-
-		return true;
+		return false;
 	}
 
 	bool ContentSizer::OnDragVertical(double verticalChange)
 	{
-		if (TargetControl() == nullptr)
+		if (const auto targetControl = TargetControl())
 		{
-			return false;
+			verticalChange = IsDragInverted() ? -verticalChange : verticalChange;
+
+			if (!IsValidHeight(targetControl, _currentSize + verticalChange, ActualHeight()))
+			{
+				return false;
+			}
+
+			targetControl.Height(_currentSize + verticalChange);
+
+			return true;
 		}
 
-		verticalChange = IsDragInverted() ? -verticalChange : verticalChange;
-
-		if (!IsValidHeight(TargetControl(), _currentSize + verticalChange, ActualHeight()))
-		{
-			return false;
-		}
-
-		TargetControl().Height(_currentSize + verticalChange);
-
-		return true;
+		return false;
 	}
 }
--- XamlToolkit.WinUI.Controls/Sizers/ContentSizer/ContentSizer.h
+++ XamlToolkit.WinUI.Controls/Sizers/ContentSizer/ContentSizer.h
@@ -7,29 +7,28 @@ namespace winrt::XamlToolkit::WinUI::Controls::implementation
 {
 	struct ContentSizer : ContentSizerT<ContentSizer, implementation::SizerBase>
 	{
-		double _currentSize;
-
 		ContentSizer();
 
-		bool IsDragInverted() { return winrt::unbox_value<bool>(GetValue(IsDragInvertedProperty)); }
+		bool IsDragInverted() const { return winrt::unbox_value<bool>(GetValue(IsDragInvertedProperty)); }
 		void IsDragInverted(bool value) { SetValue(IsDragInvertedProperty, winrt::box_value(value)); }
 
-		static inline const wil::single_threaded_property<DependencyProperty> IsDragInvertedProperty =
-			DependencyProperty::Register(L"IsDragInverted", winrt::xaml_typename<bool>(), winrt::xaml_typename<class_type>(), PropertyMetadata(winrt::box_value(false)));
+		static const wil::single_threaded_property<winrt::DependencyProperty> IsDragInvertedProperty;
 
-		FrameworkElement TargetControl() { return winrt::unbox_value<FrameworkElement>(GetValue(TargetControlProperty)); }
-		void TargetControl(FrameworkElement value) { SetValue(TargetControlProperty, value); }
+		winrt::FrameworkElement TargetControl() const { return winrt::unbox_value<winrt::FrameworkElement>(GetValue(TargetControlProperty)); }
+		void TargetControl(winrt::FrameworkElement const& value) { SetValue(TargetControlProperty, value); }
 
-		static inline const wil::single_threaded_property<DependencyProperty> TargetControlProperty =
-			DependencyProperty::Register(L"TargetControl", winrt::xaml_typename<FrameworkElement>(), winrt::xaml_typename<class_type>(), PropertyMetadata(nullptr));
+		static const wil::single_threaded_property<winrt::DependencyProperty> TargetControlProperty;
 
-		void OnLoaded(RoutedEventArgs const& e);
+		void OnLoaded(winrt::RoutedEventArgs const& e);
 
 		void OnDragStarting();
 
 		bool OnDragHorizontal(double horizontalChange);
 
 		bool OnDragVertical(double verticalChange);
+
+	private:
+		double _currentSize;
 	};
 }
 
