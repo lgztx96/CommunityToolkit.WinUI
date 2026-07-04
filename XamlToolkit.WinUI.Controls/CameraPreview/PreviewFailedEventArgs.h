f142f6ba00307d98b0ad171fe44d341f3f15ffdc
lgztx96 <z2337878191@outlook.com>
2026-07-04 17:41:32 +0800

    [PreviewFailedEventArgs] Add WIL headers for IntelliSense support

--- XamlToolkit.WinUI.Controls/CameraPreview/PreviewFailedEventArgs.h
+++ XamlToolkit.WinUI.Controls/CameraPreview/PreviewFailedEventArgs.h
@@ -2,6 +2,11 @@
 
 #include "PreviewFailedEventArgs.g.h"
 
+#ifdef __INTELLISENSE__
+#include <wil/wistd_type_traits.h>
+#include <wil/cppwinrt_authoring.h>
+#endif
+
 namespace winrt::XamlToolkit::WinUI::Controls::implementation
 {
     struct PreviewFailedEventArgs : PreviewFailedEventArgsT<PreviewFailedEventArgs>
