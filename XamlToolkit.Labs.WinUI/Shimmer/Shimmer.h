152dd902de072148ab7047cc83ba0f854bf6b8ce
lgztx96 <z2337878191@outlook.com>
2026-07-10 22:39:04 +0800

    [Shimmer] Code cleanup

--- XamlToolkit.Labs.WinUI/Shimmer/Shimmer.h
+++ XamlToolkit.Labs.WinUI/Shimmer/Shimmer.h
@@ -11,91 +11,73 @@
 
 namespace winrt
 {
+	using namespace Windows::Foundation;
+	using namespace Windows::Foundation::Numerics;
 	using namespace Microsoft::UI::Xaml;
 	using namespace Microsoft::UI::Xaml::Controls;
 	using namespace Microsoft::UI::Composition;
 	using namespace Microsoft::UI::Xaml::Hosting;
 	using namespace Microsoft::UI::Xaml::Shapes;
-	using namespace winrt::Windows::Foundation::Numerics;
 }
 
 namespace winrt::XamlToolkit::Labs::WinUI::implementation
 {
 	struct Shimmer : ShimmerT<Shimmer>
 	{
-	private:
 		static constexpr float InitialStartPointX = -7.92f;
 		static constexpr std::wstring_view PART_Shape = L"Shape";
-
-		ExpressionAnimation _sizeAnimation{ nullptr };
-		Vector2KeyFrameAnimation _gradientStartPointAnimation{ nullptr };
-		Vector2KeyFrameAnimation _gradientEndPointAnimation{ nullptr };
-		CompositionColorGradientStop _gradientStop1{ nullptr };
-		CompositionColorGradientStop _gradientStop2{ nullptr };
-		CompositionColorGradientStop _gradientStop3{ nullptr };
-		CompositionColorGradientStop _gradientStop4{ nullptr };
-		CompositionRoundedRectangleGeometry _rectangleGeometry{ nullptr };
-		ShapeVisual _shapeVisual{ nullptr };
-		CompositionLinearGradientBrush _shimmerMaskGradient{ nullptr };
-		Border _shape{ nullptr };
-
-		bool _initialized;
-		bool _animationStarted;
-
-		FrameworkElement::ActualThemeChanged_revoker _actualThemeChangedRevoker;
-	public:
 
 		Shimmer();
 
 		void OnApplyTemplate();
 
-		static void PropertyChanged(DependencyObject const& s, DependencyPropertyChangedEventArgs e);
+		static void PropertyChanged(winrt::DependencyObject const& s, winrt::DependencyPropertyChangedEventArgs e);
 
-		static inline const wil::single_threaded_property<winrt::Microsoft::UI::Xaml::DependencyProperty> DurationProperty =
-			winrt::Microsoft::UI::Xaml::DependencyProperty::Register(
+		static inline const wil::single_threaded_property<winrt::DependencyProperty> DurationProperty =
+			winrt::DependencyProperty::Register(
 				L"Duration",
-				winrt::xaml_typename<winrt::Windows::Foundation::TimeSpan>(),
+				winrt::xaml_typename<winrt::TimeSpan>(),
 				winrt::xaml_typename<class_type>(),
-				winrt::Microsoft::UI::Xaml::PropertyMetadata
+				winrt::PropertyMetadata
 				{
-					winrt::box_value(winrt::Windows::Foundation::TimeSpan{ std::chrono::milliseconds(1600) }),
+					winrt::box_value(winrt::TimeSpan{ std::chrono::milliseconds(1600) }),
 					&Shimmer::PropertyChanged
 				}
 			);
 
-		winrt::Windows::Foundation::TimeSpan Duration() const
+		winrt::TimeSpan Duration() const
 		{
-			return winrt::unbox_value<winrt::Windows::Foundation::TimeSpan>(GetValue(DurationProperty));
+			return winrt::unbox_value<winrt::TimeSpan>(GetValue(DurationProperty()));
 		}
 
-		void Duration(winrt::Windows::Foundation::TimeSpan const& value)
+		void Duration(winrt::TimeSpan const& value)
 		{
-			SetValue(DurationProperty, winrt::box_value(value));
+			SetValue(DurationProperty(), winrt::box_value(value));
 		}
 
-		static inline const wil::single_threaded_property<winrt::Microsoft::UI::Xaml::DependencyProperty> IsActiveProperty =
-			winrt::Microsoft::UI::Xaml::DependencyProperty::Register(
+		static inline const wil::single_threaded_property<winrt::DependencyProperty> IsActiveProperty =
+			winrt::DependencyProperty::Register(
 				L"IsActive",
 				winrt::xaml_typename<bool>(),
 				winrt::xaml_typename<class_type>(),
-				winrt::Microsoft::UI::Xaml::PropertyMetadata{ winrt::box_value(true), &Shimmer::PropertyChanged });
+				winrt::PropertyMetadata{ winrt::box_value(true), &Shimmer::PropertyChanged });
 
 		bool IsActive() const
 		{
-			return winrt::unbox_value<bool>(GetValue(IsActiveProperty));
+			return winrt::unbox_value<bool>(GetValue(IsActiveProperty()));
 		}
 
 		void IsActive(bool value)
 		{
-			SetValue(IsActiveProperty, winrt::box_value(value));
+			SetValue(IsActiveProperty(), winrt::box_value(value));
 		}
 
 	private:
-		void OnLoaded(IInspectable const& sender, RoutedEventArgs const& e);
+		void OnLoaded(winrt::IInspectable const& sender, winrt::RoutedEventArgs const& e);
 
-		void OnUnloaded(IInspectable const& sender, RoutedEventArgs const& e);
+		void OnUnloaded(winrt::IInspectable const& sender, winrt::RoutedEventArgs const& e);
 
-		void OnActualThemeChanged(FrameworkElement const& sender, IInspectable const& args);
+		void OnActualThemeChanged(winrt::FrameworkElement const& sender, winrt::IInspectable const& args);
 
 		bool TryInitializationResource();
 
@@ -106,6 +88,24 @@ namespace winrt::XamlToolkit::Labs::WinUI::implementation
 		void TryStartAnimation();
 
 		void StopAnimation();
+
+	private:
+		winrt::ExpressionAnimation _sizeAnimation{ nullptr };
+		winrt::Vector2KeyFrameAnimation _gradientStartPointAnimation{ nullptr };
+		winrt::Vector2KeyFrameAnimation _gradientEndPointAnimation{ nullptr };
+		winrt::CompositionColorGradientStop _gradientStop1{ nullptr };
+		winrt::CompositionColorGradientStop _gradientStop2{ nullptr };
+		winrt::CompositionColorGradientStop _gradientStop3{ nullptr };
+		winrt::CompositionColorGradientStop _gradientStop4{ nullptr };
+		winrt::CompositionRoundedRectangleGeometry _rectangleGeometry{ nullptr };
+		winrt::ShapeVisual _shapeVisual{ nullptr };
+		winrt::CompositionLinearGradientBrush _shimmerMaskGradient{ nullptr };
+		winrt::Border _shape{ nullptr };
+
+		bool _initialized;
+		bool _animationStarted;
+
+		winrt::FrameworkElement::ActualThemeChanged_revoker _actualThemeChangedRevoker;
 	};
 }
 
