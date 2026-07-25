c0d390b9e2cb1176bae76b49dd2a9a1ae886d7f3
lgztx <z2337878191@outlook.com>
2026-07-25 20:06:25 +0800

    [XamlToolkit.WinUI] Add BitmapIconExtension

---
+++ XamlToolkit.WinUI/Extensions/Markup/BitmapIconExtension.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "winrt_module_imports.h"
+#include "BitmapIconExtension.h"
+#if __has_include("BitmapIconExtension.g.cpp")
+#include "BitmapIconExtension.g.cpp"
+#endif
+
+namespace winrt::XamlToolkit::WinUI::implementation
+{
+	winrt::IInspectable BitmapIconExtension::ProvideValue() const
+	{
+		winrt::BitmapIcon icon;
+		icon.ShowAsMonochrome(ShowAsMonochrome());
+		icon.UriSource(Source());
+		return icon;
+	}
+
+	winrt::IInspectable BitmapIconExtension::ProvideValue(
+		[[maybe_unused]] winrt::IXamlServiceProvider const& provider) const
+	{
+		return ProvideValue();
+	}
+}
---
+++ XamlToolkit.WinUI/Extensions/Markup/BitmapIconExtension.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "BitmapIconExtension.g.h"
+
+#ifdef __INTELLISENSE__
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#endif
+
+namespace winrt
+{
+	using namespace ::winrt::Windows::Foundation;
+	using namespace ::winrt::Microsoft::UI::Xaml;
+	using namespace ::winrt::Microsoft::UI::Xaml::Controls;
+}
+
+namespace winrt::XamlToolkit::WinUI::implementation
+{
+	struct BitmapIconExtension : BitmapIconExtensionT<BitmapIconExtension>
+	{
+		BitmapIconExtension() = default;
+
+		wil::single_threaded_rw_property<winrt::Uri> Source{ nullptr };
+
+		wil::single_threaded_rw_property<bool> ShowAsMonochrome{ false };
+
+		winrt::IInspectable ProvideValue() const;
+
+		winrt::IInspectable ProvideValue(winrt::IXamlServiceProvider const& provider) const;
+	};
+}
+
+namespace winrt::XamlToolkit::WinUI::factory_implementation
+{
+	struct BitmapIconExtension : BitmapIconExtensionT<BitmapIconExtension, implementation::BitmapIconExtension>
+	{};
+}
