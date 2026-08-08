8ab265ca80c11aa6a24f43728450e9eb1ff581f4
lgztx <z2337878191@outlook.com>
2026-08-09 01:13:04 +0800

    [FrameworkElementExtensions] Replace handler state map with attached property

--- XamlToolkit.WinUI/Extensions/Element/FrameworkElementExtensions.cpp
+++ XamlToolkit.WinUI/Extensions/Element/FrameworkElementExtensions.cpp
@@ -4,41 +4,58 @@
 #if __has_include("FrameworkElementExtensions.g.cpp")
 #include "FrameworkElementExtensions.g.cpp"
 #endif
+#include "../Tree/DependencyObjectExtensions.h"
 
 namespace winrt::XamlToolkit::WinUI::implementation
 {
-    void FrameworkElementExtensions::RemoveHandlers() noexcept
+    winrt::com_ptr<AncestorTracker> FrameworkElementExtensions::GetAncestorTracker(winrt::FrameworkElement const& element)
     {
-        for (auto it = _handlerStates.begin(); it != _handlerStates.end();)
-        {
-            if (!it->first.get())
-            {
-                it = _handlerStates.erase(it);
-            }
-            else
-            {
-                ++it;
-            }
-        }
+        auto value = element.GetValue(AncestorTrackerProperty());
+        return winrt::get_self<AncestorTracker>(value)->get_strong();
+    }
+
+    winrt::IInspectable FrameworkElementExtensions::GetAncestor(winrt::DependencyObject const& obj)
+    {
+        return obj.GetValue(AncestorProperty());
+    }
+
+    void FrameworkElementExtensions::SetAncestor(winrt::DependencyObject const& obj, winrt::IInspectable const& value)
+    {
+        obj.SetValue(AncestorProperty(), value);
+    }
+
+    winrt::TypeName FrameworkElementExtensions::GetAncestorType(winrt::FrameworkElement const& obj)
+    {
+        return winrt::unbox_value<winrt::TypeName>(obj.GetValue(AncestorTypeProperty()));
+    }
+
+    void FrameworkElementExtensions::SetAncestorType(winrt::FrameworkElement const& obj, winrt::TypeName const& value)
+    {
+        obj.SetValue(AncestorTypeProperty(), winrt::box_value(value));
     }
 
     void FrameworkElementExtensions::AncestorType_PropertyChanged(winrt::DependencyObject const& obj, winrt::DependencyPropertyChangedEventArgs const& args)
     {
         if (auto fe = obj.try_as<winrt::FrameworkElement>())
         {
-            RemoveHandlers();
-
             if (args.NewValue())
             {
-                auto [it, inserted] = _handlerStates.try_emplace(fe);
-                auto& handlerState = it->second;
+                auto ancestorTracker = GetAncestorTracker(fe);
 
-                if (!inserted)
+                if (!ancestorTracker)
                 {
-                    fe.Loaded(handlerState._loadedToken);
+                    ancestorTracker = winrt::make_self<AncestorTracker>();
+                    fe.SetValue(AncestorTrackerProperty(), *ancestorTracker);
+                }
+                else
+                {
+                    if (ancestorTracker->loadedToken) 
+                    { 
+                        fe.Loaded(ancestorTracker->loadedToken); 
+                    }
                 }
 
-                handlerState._loadedToken = fe.Loaded(&FrameworkElementExtensions::FrameworkElement_Loaded);
+                ancestorTracker->loadedToken = fe.Loaded(&FrameworkElementExtensions::FrameworkElement_Loaded);
 
                 if (fe.Parent())
                 {
@@ -48,30 +65,20 @@ namespace winrt::XamlToolkit::WinUI::implementation
         }
     }
 
-    winrt::DependencyObject FrameworkElementExtensions::FindAscendant(winrt::DependencyObject element, winrt::TypeName const& name)
-    {
-        while (true)
-        {
-            auto parent = winrt::VisualTreeHelper::GetParent(element);
-
-            if (!parent || winrt::get_class_name(parent) == name.Name)
-            {
-                return parent;
-            }
-
-            element = parent;
-        }
-    }
-
     void FrameworkElementExtensions::FrameworkElement_Loaded(winrt::IInspectable const& sender, [[maybe_unused]] winrt::RoutedEventArgs const& e)
     {
         if (auto fe = sender.try_as<winrt::FrameworkElement>())
         {
-            SetAncestor(fe, FindAscendant(fe, GetAncestorType(fe)));
-            if (const auto it = _handlerStates.find(fe); it != _handlerStates.end())
+            SetAncestor(fe, DependencyObjectEx::FindAscendant(fe, GetAncestorType(fe)));
+
+            if (auto ancestorTracker = GetAncestorTracker(fe))
             {
-                fe.Unloaded(it->second._unloadedToken);
-                it->second._unloadedToken = fe.Unloaded(&FrameworkElementExtensions::FrameworkElement_Unloaded);
+                if (ancestorTracker->unloadedToken)
+                {
+                    fe.Unloaded(ancestorTracker->unloadedToken);
+                }
+
+                ancestorTracker->unloadedToken = fe.Unloaded(&FrameworkElementExtensions::FrameworkElement_Unloaded);
             }
         }
     }
@@ -80,32 +87,16 @@ namespace winrt::XamlToolkit::WinUI::implementation
     {
         if (auto fe = sender.try_as<winrt::FrameworkElement>())
         {
-            if (const auto it = _handlerStates.find(fe); it != _handlerStates.end()) 
+            if (auto ancestorTracker = GetAncestorTracker(fe))
             {
-                fe.Unloaded(it->second._unloadedToken);
+                if (ancestorTracker->unloadedToken)
+                {
+                    fe.Unloaded(ancestorTracker->unloadedToken);
+                    ancestorTracker->unloadedToken = { 0 };
+                }
             }
-           
+
             SetAncestor(fe, nullptr);
         }
-    }
-
-    winrt::IInspectable FrameworkElementExtensions::GetAncestor(winrt::DependencyObject const& obj)
-    {
-        return obj.GetValue(AncestorProperty);
-    }
-
-    void FrameworkElementExtensions::SetAncestor(winrt::DependencyObject const& obj, winrt::IInspectable const& value)
-    {
-        obj.SetValue(AncestorProperty, value);
-    }
-
-    winrt::TypeName FrameworkElementExtensions::GetAncestorType(winrt::FrameworkElement const& obj)
-    {
-        return winrt::unbox_value<winrt::TypeName>(obj.GetValue(AncestorTypeProperty));
-    }
-
-    void FrameworkElementExtensions::SetAncestorType(winrt::FrameworkElement const& obj, winrt::TypeName const& value)
-    {
-        obj.SetValue(AncestorTypeProperty, winrt::box_value(value));
     }
 }
