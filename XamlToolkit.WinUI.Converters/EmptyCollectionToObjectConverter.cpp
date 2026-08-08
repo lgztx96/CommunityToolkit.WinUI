82e4dd6fff3df2ea66f12158e60367352b6f7d0b
lgztx <z2337878191@outlook.com>
2026-08-08 13:29:05 +0800

    [EmptyCollectionToObjectConverter] expose CheckValueIsEmpty as overridable and support more collection types

--- XamlToolkit.WinUI.Converters/EmptyCollectionToObjectConverter.cpp
+++ XamlToolkit.WinUI.Converters/EmptyCollectionToObjectConverter.cpp
@@ -9,21 +9,56 @@
 #include "EmptyCollectionToObjectConverter.g.cpp"
 #endif
 
+#ifdef __INTELLISENSE__
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Microsoft.UI.Xaml.Interop.h>
+#else
+import winrt.Microsoft.UI.Xaml.Interop;
+#endif
+
+namespace winrt
+{
+    using namespace ::winrt::Windows::Foundation::Collections;
+    using namespace ::winrt::Microsoft::UI::Xaml::Interop;
+    using IBindableVector = ::winrt::Microsoft::UI::Xaml::Interop::IBindableVector;
+    using IBindableVectorView = ::winrt::Microsoft::UI::Xaml::Interop::IBindableVectorView;
+    using IBindableIterable = ::winrt::Microsoft::UI::Xaml::Interop::IBindableIterable;
+}
+
 namespace winrt::XamlToolkit::WinUI::Converters::implementation
 {
     bool EmptyCollectionToObjectConverter::CheckValueIsEmpty(winrt::IInspectable const& value) const
     {
-        bool isEmpty = true;
-
-        if (value)
+        if (!value)
         {
-            if (auto iterable = value.try_as<winrt::Windows::Foundation::Collections::IIterable<winrt::IInspectable>>())
-            {
-                auto iterator = iterable.First();
-                isEmpty = !iterator.HasCurrent();
-            }
+            return true;
         }
 
-        return isEmpty;
+        if (const auto vector = value.try_as<winrt::IVector<winrt::IInspectable>>())
+        {
+            return vector.Size() == 0;
+        }
+        else if (const auto vectorView = value.try_as<winrt::IVectorView<winrt::IInspectable>>())
+        {
+            return vectorView.Size() == 0;
+        }
+        else if (const auto iterable = value.try_as<winrt::IIterable<winrt::IInspectable>>())
+        {
+            return !iterable.First().HasCurrent();
+        }
+        else if (const auto bindableVector = value.try_as<winrt::IBindableVector>())
+        {
+            return bindableVector.Size() == 0;
+        }
+        else if (const auto bindableVectorView = value.try_as<winrt::IBindableVectorView>())
+        {
+            return bindableVectorView.Size() == 0;
+        }
+        else if (const auto bindableIterable = value.try_as<winrt::IBindableIterable>())
+        {
+            return !bindableIterable.First().HasCurrent();
+        }
+
+        throw winrt::hresult_invalid_argument(L"Argument 'value' is not a supported vector.");
     }
 }
