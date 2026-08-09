60c279fde368414cdb5a7b277d6e425cb1d3a2b1
lgztx <z2337878191@outlook.com>
2026-08-09 23:57:42 +0800

    Fix IsNullOrEmptyStateTrigger leaks and expand collection support
    - Unsubscribe old collection change events when Value changes
    - Store _containerChangedToken for event cleanup
    - Add IBindableObservableVector support
    - Add empty checks for IBindableVector, IBindableVectorView, and IBindableIterable
    - Use First().HasCurrent() instead of IIterable begin/end iteration
    - Call ValueProperty explicitly to avoid implicit conversion

--- XamlToolkit.WinUI/Triggers/IsNullOrEmptyStateTrigger.cpp
+++ XamlToolkit.WinUI/Triggers/IsNullOrEmptyStateTrigger.cpp
@@ -7,79 +7,128 @@
 
 namespace winrt::XamlToolkit::WinUI::implementation
 {
-    IsNullOrEmptyStateTrigger::IsNullOrEmptyStateTrigger() { UpdateTrigger(); }
+	IsNullOrEmptyStateTrigger::IsNullOrEmptyStateTrigger() { UpdateTrigger(); }
 
-    const wil::single_threaded_property<winrt::DependencyProperty> IsNullOrEmptyStateTrigger::ValueProperty =
-        winrt::DependencyProperty::Register(
-            L"Value", 
-            winrt::xaml_typename<winrt::IInspectable>(), 
-            winrt::xaml_typename<class_type>(), 
-            winrt::PropertyMetadata(nullptr, &IsNullOrEmptyStateTrigger::OnValuePropertyChanged));
+	const wil::single_threaded_property<winrt::DependencyProperty> IsNullOrEmptyStateTrigger::ValueProperty =
+		winrt::DependencyProperty::Register(
+			L"Value",
+			winrt::xaml_typename<winrt::IInspectable>(),
+			winrt::xaml_typename<class_type>(),
+			winrt::PropertyMetadata(nullptr, &IsNullOrEmptyStateTrigger::OnValuePropertyChanged));
 
-    void IsNullOrEmptyStateTrigger::OnValuePropertyChanged(winrt::DependencyObject const& d, [[maybe_unused]] winrt::DependencyPropertyChangedEventArgs const& e)
-    {
-        winrt::get_self<IsNullOrEmptyStateTrigger>(d.as<class_type>())->UpdateTrigger();
-    }
+	void IsNullOrEmptyStateTrigger::OnValuePropertyChanged(winrt::DependencyObject const& d, [[maybe_unused]] winrt::DependencyPropertyChangedEventArgs const& e)
+	{
+		auto impl = winrt::get_self<IsNullOrEmptyStateTrigger>(d.as<class_type>())->get_strong();
 
-    void IsNullOrEmptyStateTrigger::UpdateTrigger()
-    {
-        auto val = Value();
+		if (const auto oldValue = e.OldValue())
+		{
+			if (const auto oldNotifyCollection = oldValue.try_as<winrt::INotifyCollectionChanged>())
+			{
+				oldNotifyCollection.CollectionChanged(impl->_containerChangedToken);
+			}
+			else if (const auto oldObservableVector = oldValue.try_as<winrt::IObservableVector<winrt::IInspectable>>())
+			{
+				oldObservableVector.VectorChanged(impl->_containerChangedToken);
+			}
+			else if (const auto oldObservableMap = oldValue.try_as<winrt::IObservableMap<winrt::IInspectable, winrt::IInspectable>>())
+			{
+				oldObservableMap.MapChanged(impl->_containerChangedToken);
+			}
+			else if (const auto oldBindableObservableVector = oldValue.try_as<winrt::IBindableObservableVector>())
+			{
+				oldBindableObservableVector.VectorChanged(impl->_containerChangedToken);
+			}
 
-        SetActive(IsNullOrEmpty(val));
+			impl->_containerChangedToken = { 0 };
+		}
 
-        if (val == nullptr)
-        {
-            return;
-        }
+		impl->UpdateTrigger();
+	}
 
-        // Try to listen for various notification events
-        // Starting with INotifyCollectionChanged
-        if (auto valNotifyCollection = val.try_as<winrt::INotifyCollectionChanged>())
-        {
-            valNotifyCollection.CollectionChanged({ get_weak(), [this](auto&& source, auto&&) { SetActive(IsNullOrEmpty(source)); } });
-            return;
-        }
+	void IsNullOrEmptyStateTrigger::UpdateTrigger()
+	{
+		auto val = Value();
 
-        // Not INotifyCollectionChanged, try IObservableVector
-        if (auto valObservableVector = val.try_as<winrt::IObservableVector<winrt::IInspectable>>())
-        {
-            valObservableVector.VectorChanged({ get_weak(), [this](auto&& source, auto&&) { SetActive(IsNullOrEmpty(source)); } });
-            return;
-        }
+		SetActive(IsNullOrEmpty(val));
 
-        // Not INotifyCollectionChanged, try IObservableMap
-        if (auto valObservableMap = val.try_as<winrt::IObservableMap<winrt::IInspectable, winrt::IInspectable>>())
-        {
-            valObservableMap.MapChanged({ get_weak(), [this](auto&& source, auto&&) { SetActive(IsNullOrEmpty(source)); } });
-        }
-    }
+		if (val == nullptr)
+		{
+			return;
+		}
 
-    bool IsNullOrEmptyStateTrigger::IsNullOrEmpty(winrt::IInspectable const& val)
-    {
-        if (val == nullptr)
-        {
-            return true;
-        }
+		// Try to listen for various notification events
+		// Starting with INotifyCollectionChanged
+		if (const auto valNotifyCollection = val.try_as<winrt::INotifyCollectionChanged>())
+		{
+			_containerChangedToken = valNotifyCollection.CollectionChanged({ get_weak(), [this](auto&& source, auto&&) { SetActive(IsNullOrEmpty(source)); } });
+			return;
+		}
 
-        // Object is not null, check for an empty string
-        if (auto valString = val.try_as<winrt::hstring>(); valString)
-        {
-            return valString->empty();
-        }
+		// Not INotifyCollectionChanged, try IObservableVector
+		if (const auto valObservableVector = val.try_as<winrt::IObservableVector<winrt::IInspectable>>())
+		{
+			_containerChangedToken = valObservableVector.VectorChanged({ get_weak(), [this](auto&& source, auto&&) { SetActive(IsNullOrEmpty(source)); } });
+			return;
+		}
 
-        // Object is not a string, check for an empty ICollection (faster)
-        if (auto valCollection = val.try_as<winrt::IVector<winrt::IInspectable>>())
-        {
-            return valCollection.Size() == 0;
-        }
+		// Not IObservableVector, try IObservableMap
+		if (const auto valObservableMap = val.try_as<winrt::IObservableMap<winrt::IInspectable, winrt::IInspectable>>())
+		{
+			_containerChangedToken = valObservableMap.MapChanged({ get_weak(), [this](auto&& source, auto&&) { SetActive(IsNullOrEmpty(source)); } });
+			return;
+		}
 
-        // Object is not an ICollection, check for an empty IEnumerable
-        if (auto valEnumerable = val.try_as<winrt::IIterable<winrt::IInspectable>>())
-        {
-            return valEnumerable.begin() == valEnumerable.end();
-        }
+		// Not IObservableMap, try IBindableObservableVector
+		if (const auto valObservableVector = val.try_as<winrt::IBindableObservableVector>())
+		{
+			_containerChangedToken = valObservableVector.VectorChanged({ get_weak(), [this](auto&& source, auto&&) { SetActive(IsNullOrEmpty(source)); } });
+		}
+	}
 
-        // Not null and not a known type to test for emptiness
-        return false;
-    }
+	bool IsNullOrEmptyStateTrigger::IsNullOrEmpty(winrt::IInspectable const& val)
+	{
+		if (val == nullptr)
+		{
+			return true;
+		}
+
+		// Object is not null, check for an empty string
+		if (const auto valString = val.try_as<winrt::hstring>())
+		{
+			return valString->empty();
+		}
+
+		// Object is not a hstring, check for an empty IVector (faster)
+		if (const auto valVector = val.try_as<winrt::IVector<winrt::IInspectable>>())
+		{
+			return valVector.Size() == 0;
+		}
+
+		// Object is not an IVector, check for an empty IIterable
+		if (const auto valIterable = val.try_as<winrt::IIterable<winrt::IInspectable>>())
+		{
+			return !valIterable.First().HasCurrent();
+		}
+
+		// Object is not an IIterable, check for an empty IBindableVector
+		if (const auto valBindableVector = val.try_as<winrt::IBindableVector>())
+		{
+			return valBindableVector.Size() == 0;
+		}
+
+		// Object is not an IBindableVector, check for an empty IBindableVectorView
+		if (const auto valBindableVectorView = val.try_as<winrt::IBindableVectorView>())
+		{
+			return valBindableVectorView.Size() == 0;
+		}
+
+		// Object is not an IBindableVectorView, check for an empty IBindableIterable
+		if (const auto valBindableIterable = val.try_as<winrt::IBindableIterable>())
+		{
+			return !valBindableIterable.First().HasCurrent();
+		}
+
+		// Not null and not a known type to test for emptiness
+		return false;
+	}
 }
