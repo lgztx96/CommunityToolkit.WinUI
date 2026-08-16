5763215718658c50c1bec47b7bcd173d605e98c4
lgztx <z2337878191@outlook.com>
2026-08-17 01:41:26 +0800

    Add IsNotEqualStateTrigger as the inverse of IsEqualStateTrigger

---
+++ XamlToolkit.WinUI/Triggers/IsNotEqualStateTrigger.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "IsNotEqualStateTrigger.g.h"
+
+#ifdef __INTELLISENSE__
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <wil/wistd_type_traits.h>
+#include <wil/cppwinrt_authoring.h>
+#endif
+
+namespace winrt
+{
+    using namespace Windows::Foundation;
+    using namespace Microsoft::UI::Xaml;
+}
+
+namespace winrt::XamlToolkit::WinUI::implementation
+{
+    struct IsNotEqualStateTrigger : IsNotEqualStateTriggerT<IsNotEqualStateTrigger>
+    {
+        IsNotEqualStateTrigger() = default;
+
+        void UpdateTrigger();
+
+        winrt::IInspectable Value() const { return GetValue(ValueProperty()); }
+        void Value(winrt::IInspectable const& value) const { SetValue(ValueProperty(), value); }
+
+        static const wil::single_threaded_property<winrt::DependencyProperty> ValueProperty;
+
+        winrt::IInspectable To() const { return GetValue(ToProperty()); }
+        void To(winrt::IInspectable const& value) const { SetValue(ToProperty(), value); }
+
+        static const wil::single_threaded_property<winrt::DependencyProperty> ToProperty;
+
+    private:
+        static void OnValuePropertyChanged(winrt::DependencyObject const& d, winrt::DependencyPropertyChangedEventArgs const& e);
+    };
+}
+
+namespace winrt::XamlToolkit::WinUI::factory_implementation
+{
+    struct IsNotEqualStateTrigger : IsNotEqualStateTriggerT<IsNotEqualStateTrigger, implementation::IsNotEqualStateTrigger>
+    {
+    };
+}
